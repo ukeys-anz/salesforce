ac3b48dfc29ef1d4ac0741801efbddc3ded41e8a
lanceyshianz <62575151+lanceyshianz@users.noreply.github.com>
2020-04-24 16:41:51 +1000

    [ABT-6673] Case duration lwc (#85)
    * Add account table lwc into person account page
    
    * lwc for case duration
    
    * unit test and prettier
    
    * Remove the added flexi page
    
    * Use the develop version of customerInfomation.js since I only run prettier
    
    * make pmd happy
    
    * prettier
    
    * check error.body first
    
    * move to a util class
    
    * run prettier
    
    * Fix docs
    
    Co-authored-by: Pavan Kumar Ananta <>

---
+++ force-app/main/default/lwc/caseDuration/caseDuration.js
@@ -0,0 +1,31 @@
+import { LightningElement, track, api } from "lwc";
+import getCaseDurationTime from "@salesforce/apex/CaseDurationServerController.getCaseDurationTime";
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
+
+export default class CaseDuration extends LightningElement {
+  @track durationTime;
+  @api recordId;
+
+  connectedCallback() {
+    getCaseDurationTime({ caseId: this.recordId })
+      .then(result => {
+        this.durationTime = result;
+      })
+      .catch(error => {
+        let errorMessage = "Failed to load case duration";
+        if (error.body && error.body.message) {
+          errorMessage = error.body.message;
+        }
+        this.showToast("Case Duration Load Failed", errorMessage, error);
+      });
+  }
+
+  showToast(theTitle, theMessage, theVariant) {
+    const event = new ShowToastEvent({
+      title: theTitle,
+      message: theMessage,
+      variant: theVariant
+    });
+    this.dispatchEvent(event);
+  }
+}
