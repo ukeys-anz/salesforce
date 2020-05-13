e9cceb74d02b33772ac42923b15691e7f04110eb
Peter Charalambous <peter.charalambous@anz.com>
2020-05-13 16:19:42 +1000

    Updates and code comments for component

--- force-app/main/default/lwc/releaseLog/releaseLog.js
+++ force-app/main/default/lwc/releaseLog/releaseLog.js
@@ -1,14 +1,24 @@
-import { LightningElement, wire, track } from "lwc";
+import { LightningElement, track } from "lwc";
 
 import getReleases from "@salesforce/apex/ReleaseLogServerController.getReleases";
+import countTotalRecords from "@salesforce/apex/ReleaseLogServerController.countTotalRecords";
 
 import { ShowToastEvent } from "lightning/platformShowToastEvent";
 
+//Declare default records per page
+const RECORDS_PER_PAGE = 2;
+
 export default class ReleaseLog extends LightningElement {
   @track releases;
+  @track currentPage = 1;
+  @track totalPages = null;
+  @track loadMore = true;
 
   connectedCallback() {
-    getReleases()
+    //Determine the current records to display using the current page
+    //and the initial records per page
+    let recordCount = RECORDS_PER_PAGE * this.currentPage;
+    getReleases({ recordLimit: recordCount })
       .then(result => {
         for (let i = 0; i < result.length; i++) {
           //Split the text area fields into an array by new lines
@@ -45,6 +55,22 @@ export default class ReleaseLog extends LightningElement {
         }
         this.showToast("Release Log Load Failed", errorMessage, error);
       });
+
+    //Retrieve count of total records to determine the total pages
+    //available. Only needs to be done once
+    if (!this.totalPages) {
+      countTotalRecords().then(total => {
+        if (total <= 2) {
+          this.loadMore = false;
+        }
+        this.totalPages = Math.round(total / RECORDS_PER_PAGE);
+      });
+    }
+
+    //Hide the view more button if we've run out of pages to load
+    if (this.currentPage == this.totalPages) {
+      this.loadMore = false;
+    }
   }
 
   showToast(theTitle, theMessage, theVariant) {
@@ -54,5 +80,13 @@ export default class ReleaseLog extends LightningElement {
       variant: theVariant
     });
     this.dispatchEvent(event);
+  }
+
+  //Load more records
+  loadRecords() {
+    if (this.currentPage !== this.totalPages) {
+      this.currentPage++;
+      this.connectedCallback();
+    }
   }
 }
