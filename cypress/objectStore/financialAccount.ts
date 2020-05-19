98d3238d1eca48c97ff73b714bcf85e47d2cee35
Boobalakrishnan Duraisamy <duraisab@boobalahnansmbp.home>
2020-05-20 01:45:12 +1000

    Moved Person Account Creation to new function

--- cypress/objectStore/financialAccount.ts
+++ cypress/objectStore/financialAccount.ts
@@ -23,77 +23,62 @@ export async function createFinAccountList(
   amount: number = 1,
   recordType: String = "SavingsAccount"
 ) {
-  // Check if there are existing person accounts, if not create new ones
-  var ownerId: String = "";
-  var ownerName: String = "";
-  connection.query(
-    "SELECT Id, Name FROM Account WHERE RecordType.DeveloperName = 'PersonAccount' LIMIT 1",
-    async function(err: any, result: any) {
-      if (err) {
-        return console.error(err);
-      }
-
-      ownerId = result.records[0].Id;
-      ownerName = result.records[0].Name;
-
-      if (!ownerId) {
-        // insert an account and use the retrieved id
-        var accountList: any = await createAccountList(connection, 1);
-        ownerId = accountList[0].Id;
-        ownerName = accountList[0].Name;
-      }
-      return new Promise(async resolve => {
-        var finAccounts = [];
-        var idList: any = [];
-        var recTypeId = await getRecordTypeID(connection, recordType);
-        for (var i = 0; i < amount; i++) {
-          let finAccount: IFinAccount = {
-            RecordTypeId: recTypeId,
-            Name: ownerName + " Savings",
-            FinServ__FinancialAccountType__c: faker.random.arrayElement([
-              "Savings"
-            ]),
-            FinServ__Status__c: faker.random.arrayElement([
-              "Open",
-              "Closed",
-              "On Hold",
-              "Pending"
-            ]),
-            FinServ__Ownership__c: faker.random.arrayElement(["Individual"]),
-            FinServ__Balance__c: parseFloat(faker.commerce.price(99, 9999, 2)),
-            FinServ__OpenDate__c: faker.date.past(2),
-            FinServ__FinancialAccountNumber__c: faker.finance.account(9),
-            FinServ__PrimaryOwner__c: ownerId.toString()
-          };
-          finAccounts.push(finAccount);
+  return new Promise<String>(async resolve => {
+    var ownerId: String = "";
+    var ownerName: String = "";
+    var finAccounts = [];
+    var idList: any = [];
+    var recTypeId = await getRecordTypeID(connection, recordType);
+    var accountList: any = await getPersonAccount(connection);
+    for (var i = 0; i < amount; i++) {
+      let finAccount: IFinAccount = {
+        RecordTypeId: recTypeId,
+        Name: accountList[0].Name + " Savings",
+        FinServ__FinancialAccountType__c: faker.random.arrayElement([
+          "Savings"
+        ]),
+        FinServ__Status__c: faker.random.arrayElement([
+          "Open",
+          "Closed",
+          "On Hold",
+          "Pending"
+        ]),
+        FinServ__Ownership__c: faker.random.arrayElement(["Individual"]),
+        FinServ__Balance__c: parseFloat(faker.commerce.price(99, 9999, 2)),
+        FinServ__OpenDate__c: faker.date.past(2),
+        FinServ__FinancialAccountNumber__c: faker.finance.account(9),
+        FinServ__PrimaryOwner__c: accountList[0].Id
+      };
+      finAccounts.push(finAccount);
+    }
+    //Use the connection passed as a param to create the accounts
+    connection
+      .sobject("FinServ__FinancialAccount__c")
+      .create(finAccounts, (err: any, result: any) => {
+        if (err) {
+          return console.log("error", err);
+        } else if (!result[0].success) {
+          return console.log("error", result[0].errors[0].message);
         }
-        //Use the connection passed as a param to create the accounts
+        //Loop through the result to create a list of ids
+        result.forEach((item: any) => {
+          if (item.success) {
+            idList.push(item.id);
+          }
+        });
+        //Retrieve the new Financial Accounts using the created id list and return the results
         connection
           .sobject("FinServ__FinancialAccount__c")
-          .create(finAccounts, (err: any, result: any) => {
+          .retrieve(idList, (err: any, result: any) => {
             if (err) {
               return console.log("error", err);
-            } else if (!result[0].success) {
-              return console.log("error", result[0].errors[0].message);
             }
-            //Loop through the result to create a list of ids
-            result.forEach((item: any, index: any) => {
-              idList.push(item.id);
-            });
-            //Retrieve the new Financial Accounts using the created id list and return the results
-            connection
-              .sobject("FinServ__FinancialAccount__c")
-              .retrieve(idList, (err: any, result: any) => {
-                if (err) {
-                  return console.log("error", err);
-                }
-                resolve(result);
-              });
+            resolve(result);
           });
       });
-    }
-  );
+  });
 }
+
 async function getRecordTypeID(connection: any, devName: String) {
   return new Promise<String>(resolve => {
     //Todo make this query better using JSforce methods if possible
@@ -106,6 +91,29 @@ async function getRecordTypeID(connection: any, devName: String) {
           return console.error("error", err);
         }
         resolve(result.records[0].Id);
+      }
+    );
+  });
+}
+
+async function getPersonAccount(connection: any) {
+  // Check if there are existing person accounts, if not create new ones
+  return new Promise<String>(resolve => {
+    connection.query(
+      "SELECT Id, Name FROM Account WHERE RecordType.DeveloperName = 'PersonAccount' LIMIT 1",
+      async function(err: any, result: any) {
+        if (err) {
+          return console.error(err);
+        }
+
+        if (result.records.length > 0) {
+          resolve(result.records);
+        } else {
+          // insert an account and use the retrieved id
+          var accountList: any = await createAccountList(connection, 1);
+
+          resolve(accountList);
+        }
       }
     );
   });
