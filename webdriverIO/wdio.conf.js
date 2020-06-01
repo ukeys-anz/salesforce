cd8cc8e32562cef412a9c1f81060acf52f578d4e
Peter Charalambous <peter.charalambous@anz.com>
2020-06-01 15:20:06 +1000

    Updated to make headless

--- webdriverIO/wdio.conf.js
+++ webdriverIO/wdio.conf.js
@@ -57,7 +57,7 @@ exports.config = {
       "goog:chromeOptions": {
         // to run chrome headless the following flags are required
         // (see https://developers.google.com/web/updates/2017/04/headless-chrome)
-        // args: ["--headless", "--disable-gpu"]
+        args: ["--headless", "--disable-gpu"]
       }
       // browserName: "firefox"
       // If outputDir is provided WebdriverIO can capture driver session logs
