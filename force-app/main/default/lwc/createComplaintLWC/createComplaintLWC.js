add9217911b2439645bcfde38553728aaf79372f
Hima-Anz <67941446+Hima-Anz@users.noreply.github.com>
2020-09-24 15:48:06 +1000

    Add comments for current changes in logic

--- force-app/main/default/lwc/createComplaintLWC/createComplaintLWC.js
+++ force-app/main/default/lwc/createComplaintLWC/createComplaintLWC.js
@@ -446,6 +446,9 @@ export default class CreateComplaintLWC extends NavigationMixin(
           ] = this.financialCompensation;
         }
       }
+      //If user wants to create an escalated case. Set the case status to default status and set the isEscalated flag.
+      // This is required to handle afterUpdate trigger logic.
+      // Status will be set to escalated in 'HandleCaseEscalationRules' of IDRCaseActions class from the trigger.
       if (
         this.isComplaintEscalated &&
         fields[ESCALATED_TO.fieldApiName] === "1"
