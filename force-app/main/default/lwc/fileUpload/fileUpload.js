008c7c108797ea6f5306f9f340fd0e8456217f03
Khana-anz <67884936+Khana-anz@users.noreply.github.com>
2020-10-26 11:44:14 +1100

    Prettier changes

--- force-app/main/default/lwc/fileUpload/fileUpload.js
+++ force-app/main/default/lwc/fileUpload/fileUpload.js
@@ -1,24 +1,27 @@
-import { LightningElement, api } from 'lwc';
-import {ShowToastEvent} from 'lightning/platformShowToastEvent';
+import { LightningElement, api } from "lwc";
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
 
 export default class FileUpload extends LightningElement {
-    @api recordId;
-    get acceptedFormats() {
-        return ['.pdf', '.png','.jpg','.jpeg'];
+  @api recordId;
+  get acceptedFormats() {
+    return [".pdf", ".png", ".jpg", ".jpeg"];
+  }
+  handleUploadFinished(event) {
+    // Get the list of uploaded files
+    const uploadedFiles = event.detail.files;
+    let uploadedFileNames = "";
+    for (let i = 0; i < uploadedFiles.length; i++) {
+      uploadedFileNames += uploadedFiles[i].name + ", ";
     }
-    handleUploadFinished(event) {
-        // Get the list of uploaded files
-        const uploadedFiles = event.detail.files;
-        let uploadedFileNames = '';
-        for(let i = 0; i < uploadedFiles.length; i++) {
-            uploadedFileNames += uploadedFiles[i].name + ', ';
-        }
-        this.dispatchEvent(
-            new ShowToastEvent({
-                title: 'Success',
-                message: uploadedFiles.length + ' Files uploaded Successfully: ' + uploadedFileNames,
-                variant: 'success',
-            }),
-        );
-    }
+    this.dispatchEvent(
+      new ShowToastEvent({
+        title: "Success",
+        message:
+          uploadedFiles.length +
+          " Files uploaded Successfully: " +
+          uploadedFileNames,
+        variant: "success"
+      })
+    );
+  }
 }
