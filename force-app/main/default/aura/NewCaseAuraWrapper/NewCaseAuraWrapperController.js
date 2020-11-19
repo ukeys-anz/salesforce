930e6248699281fe4b08baa53fbf180d6ac26c53
Shermal Warnakula <shermal.warnakula@anz.com>
2020-11-19 13:57:47 +1100

    Fixing console navigation issues related new Case creation

--- force-app/main/default/aura/NewCaseAuraWrapper/NewCaseAuraWrapperController.js
+++ force-app/main/default/aura/NewCaseAuraWrapper/NewCaseAuraWrapperController.js
@@ -24,16 +24,61 @@
           if (response == true) {
             workspaceAPI.getFocusedTabInfo().then(function (response) {
               var firstTabId = response.tabId;
-              workspaceAPI
-                .openConsoleURL({
-                  url:
-                    "/lightning/o/Case/new?count=1&nooverride=1&recordTypeId=" +
-                    component.get("v.pageReference").state.recordTypeId,
-                  focus: true
-                })
-                .then(function (activeTabId) {
-                  workspaceAPI.closeTab({ tabId: firstTabId });
-                });
+
+              //Reading Parent Record ID from URL to manipulate the console URL to open New Case window as a sub-tab.
+              var value = helper.getURLParameterByName(
+                component,
+                "inContextOfRef"
+              );
+
+              if (value) {
+                var context = JSON.parse(window.atob(value));
+                var parentRecID = context.attributes.recordId;
+                var parentObjectName = context.attributes.objectApiName;
+
+                if (parentRecID) {
+                  if (parentObjectName == "Account") {
+                    var defaultFieldValues =
+                      "&defaultFieldValues=AccountId=" + parentRecID;
+
+                    workspaceAPI
+                      .openConsoleURL({
+                        url:
+                          "/lightning/o/Case/new?count=1&nooverride=1&recordTypeId=" +
+                          component.get("v.pageReference").state.recordTypeId +
+                          "&ws=%2Flightning%2Fr%2F" +
+                          parentObjectName +
+                          "%2F" +
+                          parentRecID +
+                          "%2Fview" +
+                          defaultFieldValues,
+                        focus: true
+                      })
+                      .then(function (activeTabId) {
+                        workspaceAPI.closeTab({ tabId: firstTabId });
+                      });
+
+                    // TODO: Have to manually pre-pupulate default values for objects other than Account
+                    // Only addressing the console sub-tab behaviour and parent object link for now
+                  } else {
+                    workspaceAPI
+                      .openConsoleURL({
+                        url:
+                          "/lightning/o/Case/new?count=1&nooverride=1&recordTypeId=" +
+                          component.get("v.pageReference").state.recordTypeId +
+                          "&ws=%2Flightning%2Fr%2F" +
+                          parentObjectName +
+                          "%2F" +
+                          parentRecID +
+                          "%2Fview",
+                        focus: true
+                      })
+                      .then(function (activeTabId) {
+                        workspaceAPI.closeTab({ tabId: firstTabId });
+                      });
+                  }
+                }
+              }
             });
           } else {
             helper.goToStandardNewCasePage(component, event);
