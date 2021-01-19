4a8cb6334163f9b063187ae449798b3842fdc291
jasmine-anzx <74887559+jasmine-anzx@users.noreply.github.com>
2021-01-19 17:24:15 +1100

    Add utils module (#720)
    * Added utils.js module
    
    * Added utils meta file

---
+++ force-app/main/default/lwc/utils/utils.js
@@ -0,0 +1,58 @@
+/**
+ *
+ * @author Jasmine Nguyen
+ * @since 12/2020
+ * @description Utils module which stores methods that can be shared between Lightning Web Components
+ */
+
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
+
+// Hanlde show toast message
+export function showToast(
+  cmp,
+  theTitle,
+  theMessage,
+  theMessageData,
+  theVariant,
+  theMode
+) {
+  const event = new ShowToastEvent({
+    title: theTitle,
+    message: theMessage,
+    variant: theVariant,
+    messageData: theMessageData,
+    mode: theMode ? theMode : "dismissable"
+  });
+
+  cmp.dispatchEvent(event);
+}
+
+// Hanlde errors which are returned as stringified json
+export function handleStringifiedError(error) {
+  try {
+    JSON.parse(error);
+  } catch (e) {
+    return error;
+  }
+  return JSON.parse(error).message;
+}
+
+// Handle error and showToast
+export function handleErrorShowToast(
+  cmp,
+  title,
+  error,
+  defaultErrorMessage,
+  mode
+) {
+  let errorMessage = defaultErrorMessage;
+  if (error && error.body && error.body.message) {
+    let message = handleStringifiedError(error.body.message);
+    //Catch any system error messages (most readable errors wont be a single word)
+    if (message && message.split(" ").length > 1) {
+      errorMessage = message;
+    }
+  }
+  cmp.hasError = true;
+  showToast(cmp, title, errorMessage, "", "error", mode);
+}
