7ca65c00794556ae2548ba97bebff74f147faa7e
lanceyshianz <62575151+lanceyshianz@users.noreply.github.com>
2021-01-20 15:56:06 +1100

    Feature/abt 23689 accounts and goals jest tests (#685)
    * Add initial Accounts Balance test
    
    * Add another test suite
    
    * Add coverage line
    
    * and loading test and normal test
    
    * Add subscribe method verification
    
    * Add different test methods
    
    * Add some new refresh tests
    
    * Add every account tests
    
    * Add mock stub to make everything work
    
    * Add more coverage
    
    * add everyday account and total balance test suite
    
    * Add financial goals test
    
    * Add accountsAndGoalsRefresh test
    
    * run prettier
    
    * fix accountBalances jest test
    
    * Add negative tests

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 const { jestConfig } = require("@salesforce/sfdx-lwc-jest/config");
 module.exports = {
-  ...jestConfig
-  // add any custom configurations here
+  ...jestConfig,
+  moduleNameMapper: {
+ '^lightning/messageService$':
+     '<rootDir>/force-app/tests/jest-mocks/lightning/messageService'
+   }
 };
