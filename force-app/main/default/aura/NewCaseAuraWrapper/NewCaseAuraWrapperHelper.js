e94ddd1262621ba4a9bbdfe03ea354de30edad11
sujan patel <sujan.patel@anz.com>
2021-02-01 12:47:46 +1100

    refactored new case creation aura controller code from a mammoth single function to readable chunks

--- force-app/main/default/aura/NewCaseAuraWrapper/NewCaseAuraWrapperHelper.js
+++ force-app/main/default/aura/NewCaseAuraWrapper/NewCaseAuraWrapperHelper.js
@@ -1,5 +1,3 @@
-import { open } from "inspector";
-
 ({
   getRtDevName: function (component, callback) {
     // Identify which record type was selected
@@ -35,58 +33,55 @@ import { open } from "inspector";
     this.handleCaseRecordOpenEvent(component, "")
   },
   handleCaseRecordOpenEvent: function(component, recordTypeId){
-    var newCaseRecord = $A.get("e.force:createRecord");
 
     // Read from URL param 'inContextOfRef' to identify parent record ID
-    var value = this.getURLParameterByName(component, "inContextOfRef");
-    if (value) {
-      
-      var parentInfo = extractParentInfo(value)
+    var parentIdParameter = this.getURLParameterByName(component, "inContextOfRef");
+
+      var parentInfo = this.extractParentInfo(parentIdParameter)
       // Manually populating the related parent Account record ID when new case creation was originated from a related list
-      if (parentInfo.parentRecID && parentInfo.parentObjectName == "Account") {            
+      if (this.isParentObjectAccount(parentInfo)) {            
         
 
           // Fetch active Chat Topic ID (if there is any) related to the Customer
-          this.getActiveChatTopicID(component, parentRecID, this.getCaseRecordOpenBaseonChatCallBack(recordTypeId));
+          this.getActiveChatTopicID(component, parentInfo.parentRecID, this.getCaseRecordOpenBaseonChatCallBack(component,parentInfo.parentRecID,recordTypeId));
         } 
        else {
-         this.openCaseRecordPage(recordTypeId);        
+         this.openCaseRecordPage(recordTypeId,{});        
       }
-    }
   },
   // Pre-populate Chat Topic lookup if parent object is Account     
   extractParentInfo: function(urlParameter){
-      var context = JSON.parse(window.atob(value));
+    if(urlParameter)
+    {
+      var context = JSON.parse(window.atob(urlParameter));
 
       return { parentRecID : context.attributes.recordId,
          parentObjectName : context.attributes.objectApiName
       }
+    }else 
+    return {}
   },
-  getCaseRecordOpenBaseonChatCallBack: function(recordTypeId){
+  getCaseRecordOpenBaseonChatCallBack: function(component,parentRecID,recordTypeId){
     
-   component.set("v.loading", true);
+   this.startSpinner(component)
 
-   function openCaseRecordPageBasedOnChatTopic(topicID){
-      //Setting 'defaultFieldValues' to pre-populate the parent Account lookup.
-      //Also pre-populating/linking Chat Topic record, if the Case is related to an on-going active chat.
+   return topicID => {
       var defaultFieldValues = {};
       if (topicID) {
         
-        if (isSingleChatTopic(topicID)) {
-      
-          defaultFieldValues =  {
-              AccountId: parentRecID,
-              Twilio_Channel_SID__c: topicID,
-              Origin: "Chat"
-            }
+        if (this.isMultipleChatTopic(topicID)) {          
+          defaultFieldValues = {
+            AccountId: parentRecID,
+            Auto_matched_Chat_Topic_IDs__c: topicID
+          }              
       
         } else {
-          // only comes here if array length > 1
-          // Saving '|' separated IDs on a custom field to help in troubleshooting           
-            defaultFieldValues = {
-              AccountId: parentRecID,
-              Auto_matched_Chat_Topic_IDs__c: topicID
-            }               
+                 
+          defaultFieldValues =  {
+            AccountId: parentRecID,
+            Twilio_Channel_SID__c: topicID,
+            Origin: "Chat"
+          }           
         }
 
       } else {
@@ -97,14 +92,15 @@ import { open } from "inspector";
       }
       this.openCaseRecordPage(recordTypeId, defaultFieldValues);
       // Stop loading spinner
-      component.set("v.loading", false);
+       this.stopSpinner(component)
     }
-    return openCaseRecordPageBasedOnChatTopic
+   
   },
-  isSingleChatTopic: function(topicID){
-    return topicID.split("|").length == 1;
+  isMultipleChatTopic: function(topicID){
+    return topicID.split("|").length > 1;
   },
-  openCaseRecordPage: function(recordTypeId = "", defaultFieldValuesObj = {} ){
+  openCaseRecordPage: function(recordTypeId, defaultFieldValuesObj){
+    var newCaseRecord = $A.get("e.force:createRecord");
     var paramValue = {
       entityApiName: "Case",
       defaultFieldValues: defaultFieldValuesObj
@@ -150,6 +146,7 @@ import { open } from "inspector";
         console.log("Failed with state: " + state);
       }
     });
+    this.startSpinner(component);
     $A.enqueueAction(action);
   },
   navigateToNewCaseClosePreviousTab: function (
@@ -174,5 +171,135 @@ import { open } from "inspector";
       type: notificationType
     });
     toastEvent.fire();
+  },
+  startSpinner: function(component){
+    component.set("v.loading", true);
+  },
+  stopSpinner: function(component){
+    if (component.get("v.loading")) {
+      component.set("v.loading", false);
+    }
+  },
+  addParentInfoToUrl: function(navigationUrl, parentInfo){
+    return navigationUrl +
+    "&ws=%2Flightning%2Fr%2F" +
+    parentInfo.parentObjectName +
+    "%2F" +
+    parentInfo.parentRecID +
+    "%2Fview";
+
+  },
+  isParentObjectAccount: function(parentInfo){
+    return parentInfo.parentRecID && parentInfo.parentObjectName == "Account"
+  },
+  isComplaintCase: function(recordType){
+    return recordType == "Non_Customer_Complaint" || recordType == "Customer_Complaint"
+  },
+  setComplaintParameters: function(component, recordType){
+    component.set(
+      "v.recordTypeId",
+      component.get("v.pageReference").state.recordTypeId
+    );
+    component.set("v.recordTypeDevName", recordType);
+    component.set("v.showComponent", true);
+  },
+  handleNonComplaintCase: function(component){
+    let workspaceAPI = component.find("workspace");
+
+    workspaceAPI.isConsoleNavigation().then(isConsole => {
+      if(isConsole){
+        this.handleConsoleAppCaseCreation(component, workspaceAPI)
+      }else {
+        this.goToStandardNewCasePage(component, event);
+      }
+    })    
+  },
+  handleConsoleAppCaseCreation: function(component, workspaceAPI){
+       workspaceAPI.getFocusedTabInfo().then(firstTabId => {
+
+      //Reading Parent Record ID from URL to manipulate the console URL to open New Case window as a sub-tab.
+      var parentIdParameter = this.getURLParameterByName(
+        component,
+        "inContextOfRef"
+      );
+
+      var navigationUrl =
+        "/lightning/o/Case/new?count=1&nooverride=1&recordTypeId=" +
+        component.get("v.pageReference").state.recordTypeId;
+
+     
+      var parentInfo = this.extractParentInfo(parentIdParameter);
+
+      if (parentInfo.parentRecID) {
+        navigationUrl = this.addParentInfoToUrl(navigationUrl, parentInfo)
+      }
+          
+
+      if (this.isParentObjectAccount(parentInfo)) {
+        this.handleConsoleCaseCreationFromAccountPage(component,workspaceAPI, navigationUrl, parentInfo, firstTabId)
+      }else{
+
+         this.navigateToNewCaseClosePreviousTab(
+            workspaceAPI,
+            navigationUrl,
+            firstTabId
+          )
+      }
+    })
+    
+  },
+  handleConsoleCaseCreationFromAccountPage: function(component, workspaceAPI, navigationUrl, parentInfo, tabId){
+    //Setting 'defaultFieldValues' to pre-populate the parent Account lookup.
+    navigationUrl = navigationUrl +"&defaultFieldValues=AccountId=" +  parentInfo.parentRecID
+
+    this.getActiveChatTopicID(
+      component,
+      parentInfo.parentRecID,
+      this.handleConsoleChatCaseCreationCallback(component,navigationUrl, workspaceAPI, tabId)
+    );
+  },
+  handleConsoleChatCaseCreationCallback: function(component,navigationUrl, workspaceAPI, tabId){
+    return (topicID) => {
+      //Also pre-populating/linking Chat Topic record, if the Case is related to an on-going active chat.
+      var defaultFieldValues = "";
+      if (topicID) {
+   
+        if (this.isMultipleChatTopic(topicID)) {
+          navigationUrl =
+            navigationUrl +
+            ",Auto_matched_Chat_Topic_IDs__c=" +
+            topicID;
+
+          this.showToast(
+            "warning",
+            "More than one Active Chat Topics found ! Chat Topic will not be auto-populated on Case."
+          );
+         
+        } else {
+          navigationUrl =
+          navigationUrl +
+          ",Twilio_Channel_SID__c=" +
+          topicID +
+          ",Origin=Chat";
+
+        this.showToast(
+          "success",
+          "Active Chat Topic was successfully auto-populated on Case !"
+        );
+        }
+      }
+
+      // Stop loading spinner
+      this.stopSpinner(component)
+
+      this.navigateToNewCaseClosePreviousTab(
+        workspaceAPI,
+        navigationUrl,
+        tabId
+      );
+    }
   }
-});
+  
+})
+
+
