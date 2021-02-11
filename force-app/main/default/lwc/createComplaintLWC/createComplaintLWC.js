f3d6fb1100789796d54f9f1723cb9abf126b68c8
Khana-anz <67884936+Khana-anz@users.noreply.github.com>
2021-02-11 15:31:27 +1100

    Removed Escalated to from UI and added default Queue in the Assignment rule

--- force-app/main/default/lwc/createComplaintLWC/createComplaintLWC.js
+++ force-app/main/default/lwc/createComplaintLWC/createComplaintLWC.js
@@ -529,11 +529,12 @@ export default class CreateComplaintLWC extends NavigationMixin(
       // This is required to handle afterUpdate trigger logic.
       // Status will be set to escalated in 'HandleCaseEscalationRules' of IDRCaseActions class from the trigger.
       if (
-        this.isComplaintEscalated &&
-        fields[ESCALATED_TO.fieldApiName] === "1"
+        this.isComplaintEscalated
       ) {
-        fields[STATUS_FIELD.fieldApiName] = OPEN_STATUS_API_NAME;
+        //fields[STATUS_FIELD.fieldApiName] = OPEN_STATUS_API_NAME;
+        console.log('Afreen');
         fields.IsEscalated = true;
+        fields[ESCALATED_TO.fieldApiName] = "1";
       }
       if (this.isRealFormNeeded) {
         fields[IS_REAL_FORM_NEED_FIELD.fieldApiName] = true;
