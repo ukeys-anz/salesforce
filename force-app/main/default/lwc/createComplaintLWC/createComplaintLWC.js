c5ec2e12542d3ca1782fc926919602635164b401
Khana-anz <67884936+Khana-anz@users.noreply.github.com>
2021-02-23 15:26:34 +1100

    Addressing review Comments

--- force-app/main/default/lwc/createComplaintLWC/createComplaintLWC.js
+++ force-app/main/default/lwc/createComplaintLWC/createComplaintLWC.js
@@ -676,7 +676,8 @@ export default class CreateComplaintLWC extends NavigationMixin(
   validateComplaintPriority() {
     if (
       this.ComplaintPriority !== "Financial Hardship" &&
-      this.ComplaintPriority !== "Customer in Collections"
+      this.ComplaintPriority !== "Customer in Collections" &&
+      this.ComplaintPriority !== "Customer experiencing financial hardship"
     ) {
       this.missingDataFields +=
         "Priority must be Customer experiencing financial hardship or Customer in Collections, when collections flag is turned on.";
