afa222766ab89520a7935639f849d67b6e822abd
Peter Charalambous <peter.charalambous@anz.com>
2021-06-21 12:05:43 +1000

    Added timezone time checks for midnight

--- force-app/main/default/lwc/transactionHistoryBoard/transactionHistoryBoard.js
+++ force-app/main/default/lwc/transactionHistoryBoard/transactionHistoryBoard.js
@@ -348,6 +348,7 @@ export default class TransactionHistoryBoard extends LightningElement {
   handleSearch() {
     if (this.startDate && this.endDate) {
       this.loading = true;
+      let convertedTimes = this.getUTCTimeFromTimezone();
 
       //Create dates based off the user selection
       let startDate = new Date(this.startDate + " 00:00:00");
@@ -358,13 +359,17 @@ export default class TransactionHistoryBoard extends LightningElement {
       let utcStartDate = new Date(
         `${
           startDate.getUTCMonth() + 1
-        }-${startDate.getUTCDate()}-${startDate.getUTCFullYear()} 00:00:00 UTC`
+        }-${startDate.getUTCDate()}-${startDate.getUTCFullYear()} ${
+          convertedTimes.startTime
+        } UTC`
       ).toISOString();
 
       let utcEndDate = new Date(
         `${
           endDate.getUTCMonth() + 1
-        }-${endDate.getUTCDate()}-${endDate.getUTCFullYear()} 00:00:00 UTC`
+        }-${endDate.getUTCDate()}-${endDate.getUTCFullYear()} ${
+          convertedTimes.endTime
+        } UTC`
       ).toISOString();
 
       //Need to convert dates to ISO string for search params
@@ -395,6 +400,81 @@ export default class TransactionHistoryBoard extends LightningElement {
       this.disableSearch = false;
     } else {
       this.disableSearch = true;
+    }
+  }
+
+  //This function retrieves the UTC time equivalent of midnight from
+  //the users current timezone
+  getUTCTimeFromTimezone() {
+    let daylightSavings = this.isDaylightSavings();
+    //Get city from timezone used in Salesforce
+    let city = userTimezone.replace("Australia/", "");
+
+    //Check city to return start and end times
+    //of UTC equivalent of midnight of current location
+    switch (city) {
+      case "Sydney":
+      case "Melbourne":
+      case "Hobart":
+      case "Canberra":
+        if (daylightSavings) {
+          return { startTime: "13:00:00", endTime: "12:59:59" };
+        } else {
+          return { startTime: "14:00:00", endTime: "13:59:59" };
+        }
+      case "Brisbane":
+        return { startTime: "14:00:00", endTime: "13:59:59" };
+      case "Adelaide":
+      case "Broken Hill":
+        if (daylightSavings) {
+          return { startTime: "13:30:00", endTime: "12:29:59" };
+        } else {
+          return { startTime: "14:30:00", endTime: "14:29:59" };
+        }
+      case "Darwin":
+        return { startTime: "14:30:00", endTime: "14:29:59" };
+      case "Perth":
+        return { startTime: "16:00:00", endTime: "15:59:59" };
+      default:
+        return { startTime: "00:00:00", endTime: "23:59:59" };
+    }
+  }
+
+  //Check if we should be using AEST or AEDT
+  //AEDT begins first Sunday of October until first Sunday of April
+  isDaylightSavings() {
+    let today = new Date();
+    let daylightDate = new Date();
+
+    //Set month to April and get first Sunday of month
+    daylightDate.setMonth(3);
+    let aprilDate = new Date(
+      daylightDate.getFullYear(),
+      daylightDate.getMonth(),
+      1,
+      0,
+      0,
+      0
+    );
+    aprilDate.setDate(aprilDate.getDate() + 7 - aprilDate.getDay());
+
+    //Set month to October and get first Sunday of month
+    daylightDate.setMonth(9);
+    let octoberDate = new Date(
+      daylightDate.getFullYear(),
+      daylightDate.getMonth(),
+      1,
+      0,
+      0,
+      0
+    );
+    octoberDate.setDate(octoberDate.getDate() + 7 - octoberDate.getDay());
+
+    //Check if we are in AEST or AEDT
+    if (today > aprilDate && today < octoberDate) {
+      return false;
+    } else {
+      return true;
     }
   }
 
