a09f45f1a5dc929d18fb3dcb0d8994f504ad2605
Pavan Kumar Ananta <62244546+pavankumarananta@users.noreply.github.com>
2021-07-20 12:06:14 +1000

    ABT-0 changes for CHG0290544 for July 21st release (#1539)
    * [ABT-48076] Modifying Engineer role perms to support SF/Sailpoint user provisioning (#1495)
    
    * Modifying Engineer roler perms to spport SF/Sailpoint user provisioning
    
    * Adding Engineer Role on UPR settings
    
    * FraudX Export Report (#1522)
    
    FraudX Report Lens
    
    * Updated LWC for balance removals
    
    * Updated jest tests
    
    * remove old cmos sharing rule from git
    
    * [ABT-48156] Change colour on My Dashboard (#1521)
    
    * Support Coach View for NPS (#1496)
    
    * [ABT-47751] Restore KYC QA Dashboard (#1502)
    
    * Hotfix EA CMOS Dashboards
    
    Co-authored-by: Shermal Warnakula <62268058+warnakuw@users.noreply.github.com>
    Co-authored-by: Mo Assafiri <73924151+assafirm@users.noreply.github.com>
    Co-authored-by: Peter Charalambous <peter.charalambous@anz.com>
    Co-authored-by: boobalakrishnan duraisamy <boobalakrishnan.duraisamy@anz.com>
    Co-authored-by: Bala <62235340+duraisab@users.noreply.github.com>
    Co-authored-by: Peter Charalambous <62224246+PeterCharalambousANZ@users.noreply.github.com>
    Co-authored-by: Mouhamed Assaffri <mouhamed.assafiri@anz.com>

--- force-app/main/default/lwc/everydayAccount/everydayAccount.js
+++ force-app/main/default/lwc/everydayAccount/everydayAccount.js
@@ -26,6 +26,7 @@ export default class EverydayAccount extends NavigationMixin(LightningElement) {
   hasError = false;
   error;
   componentTitle;
+  balanceTitle;
 
   @wire(MessageContext)
   messageContext;
@@ -38,6 +39,10 @@ export default class EverydayAccount extends NavigationMixin(LightningElement) {
 
   connectedCallback() {
     this.componentTitle = ACCOUNT_TYPES[this.accountType.toLowerCase()];
+    this.balanceTitle =
+      this.accountType.toLowerCase() === "checking"
+        ? "Everyday Funds"
+        : "Total Saved";
     if (hasAccountsGoalsPermission) {
       this.loadingSubscription = subscribe(
         this.messageContext,
@@ -139,13 +144,6 @@ export default class EverydayAccount extends NavigationMixin(LightningElement) {
               style: "currency",
               currency: "AUD"
             }).format(finAccount.FinServ__Balance__c);
-            finAccount.FinServ__CurrentPostedBalance__c = new Intl.NumberFormat(
-              "en-AU",
-              {
-                style: "currency",
-                currency: "AUD"
-              }
-            ).format(finAccount.FinServ__CurrentPostedBalance__c);
           });
         }
         this.financialAccounts = result;
