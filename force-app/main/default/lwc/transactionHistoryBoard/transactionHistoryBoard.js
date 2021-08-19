32b3ae42fedbebe534f3b5c9ab3e18526fd3774b
sujan patel <sujan.patel@anz.com>
2021-08-19 23:01:19 +1000

    simplify set amount & currency function

--- force-app/main/default/lwc/transactionHistoryBoard/transactionHistoryBoard.js
+++ force-app/main/default/lwc/transactionHistoryBoard/transactionHistoryBoard.js
@@ -121,7 +121,6 @@ export default class TransactionHistoryBoard extends LightningElement {
       this.loading = false;
       return;
     }
-
     getTransactions({
       ocvId: this.ocvId,
       accountNumber: this.accountNumber,
@@ -146,7 +145,7 @@ export default class TransactionHistoryBoard extends LightningElement {
               ];
 
               // Get charged, converted, exchangeRate amounts
-              this.setAmountByType(currentTransaction, "charged");
+              this.setAmountByType(currentTransaction, "charged", 0);
               this.setAmountByType(currentTransaction, "converted");
               this.setAmountByType(currentTransaction, "exchangeRate");
 
@@ -164,8 +163,8 @@ export default class TransactionHistoryBoard extends LightningElement {
 
               //Process date and time, set showDateTitle
               let currentDate = this.getDateObject(
-                  currentTransaction.transactionDateLocal
-                );
+                currentTransaction.transactionDateLocal
+              );
 
               this.setTransactionDisplayDateTime(currentTransaction);        
               /* 
@@ -494,21 +493,20 @@ export default class TransactionHistoryBoard extends LightningElement {
   }
 
   // Based on the type, get the according amount
-  setAmountByType(transaction, amountType) {
+  setAmountByType(transaction, amountType, defaultValue = 'Unknown') {
     if (
-      transaction.amount &&
-      transaction.amount[`${amountType}`] &&
-      transaction.amount[`${amountType}`].value
+      transaction.amount?.[amountType]?.value
     ) {
-      transaction.amount[`${amountType}`].value = parseFloat(
-        transaction.amount[`${amountType}`].value,
+      transaction.amount[amountType].value = parseFloat(
+        transaction.amount[amountType].value,
         10
       ).toFixed(2);
     } else {
-      transaction.amount[`${amountType}`] = {}; // Have to create the nested object before create a property
-      transaction.amount[`${amountType}`].value =
-        amountType === "charged" ? 0 : "Unknown";
-    }
+      transaction.amount[amountType] = {
+        value: defaultValue
+      }; 
+     
+     }
     return transaction;
   }
 
@@ -516,11 +514,11 @@ export default class TransactionHistoryBoard extends LightningElement {
   setCurrencyByType(transaction, currencyType) {
     if (
       transaction.amount &&
-      (!transaction.amount[`${currencyType}`] ||
-        !transaction.amount[`${currencyType}`].currencyCode)
+      !transaction.amount?.[currencyType]?.currencyCode 
     ) {
-      transaction.amount[`${currencyType}`] = {}; // Have to create the nested object before create a property
-      transaction.amount[`${currencyType}`].currencyCode = "Unknown";
+      transaction.amount[currencyType] = {
+        currencyCode : "Unknown"
+      }; 
     }
     return transaction;
   }
