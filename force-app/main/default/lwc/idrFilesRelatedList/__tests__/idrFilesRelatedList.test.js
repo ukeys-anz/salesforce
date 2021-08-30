4590d233c452950c4224df1b05428a1a8dcf3d0e
mohama20 <adnanuddin.mohammed@anz.com>
2021-08-30 22:49:57 +1000

    Update jest for contains search and sorting test

--- force-app/main/default/lwc/idrFilesRelatedList/__tests__/idrFilesRelatedList.test.js
+++ force-app/main/default/lwc/idrFilesRelatedList/__tests__/idrFilesRelatedList.test.js
@@ -1,14 +1,27 @@
 import { createElement } from "lwc";
 import IdrFilesRelatedList from "c/idrFilesRelatedList";
 import getCaseRelatedFiles from "@salesforce/apex/IDRFilesRelatedListController.getCaseRelatedFiles";
+import searchFilesContent from "@salesforce/apex/IDRFilesRelatedListController.searchFilesContent";
 import { ShowToastEventName } from "lightning/platformShowToastEvent";
 
 const RECORD_ID = "a0c2O00000197sUQAD";
 const mockGetCaseRelatedFiles = require("./data/getCaseRelatedFiles.json");
 const mockGetCaseRelatedFilesError = require("./data/getCasesRelatedFilesError.json");
+const mockSearchFilesContent = require("./data/searchFilesContent.json");
+const mockSearchFilesContentError = require("./data/getCasesRelatedFilesError.json");
 
 jest.mock(
   "@salesforce/apex/IDRFilesRelatedListController.getCaseRelatedFiles",
+  () => {
+    return {
+      default: jest.fn()
+    };
+  },
+  { virtual: true }
+);
+
+jest.mock(
+  "@salesforce/apex/IDRFilesRelatedListController.searchFilesContent",
   () => {
     return {
       default: jest.fn()
@@ -80,11 +93,9 @@ describe("c-idr-files-related-list suite", () => {
     element.addEventListener(ShowToastEventName, handler);
     return new Promise(setImmediate).then(() => {
       expect(handler).toHaveBeenCalled();
-      //expect(handler.mock.calls[0][0].detail.title).toBe(mockGetCaseRelatedFilesError.title);
       expect(handler.mock.calls[0][0].detail.message).toBe(
         mockGetCaseRelatedFilesError.body.message
       );
-      //expect(handler.mock.calls[0][0].detail.variant).toBe(mockGetCaseRelatedFilesError.variant);
     });
   });
 
@@ -107,7 +118,9 @@ describe("c-idr-files-related-list suite", () => {
         const dataTableElement = element.shadowRoot.querySelector(
           "lightning-datatable"
         );
-        expect(dataTableElement.data[0]).toStrictEqual(mockGetCaseRelatedFiles[0]);
+        expect(dataTableElement.data[0]).toStrictEqual(
+          mockGetCaseRelatedFiles[0]
+        );
       });
     });
   });
@@ -130,11 +143,13 @@ describe("c-idr-files-related-list suite", () => {
         const dataTableElement = element.shadowRoot.querySelector(
           "lightning-datatable"
         );
-        expect(dataTableElement.data[0]).toStrictEqual(mockGetCaseRelatedFiles[0]);
+        expect(dataTableElement.data[0]).toStrictEqual(
+          mockGetCaseRelatedFiles[0]
+        );
       });
     });
   });
-  
+
   it("search files from file owner search", () => {
     getCaseRelatedFiles.mockResolvedValue(mockGetCaseRelatedFiles);
     const element = createElement("c-idr-files-related-list", {
@@ -170,13 +185,15 @@ describe("c-idr-files-related-list suite", () => {
       const fileStartDateSearchElement = element.shadowRoot.querySelector(
         ".fileStartDateSearch"
       );
-      fileStartDateSearchElement.value = '2021-08-30';
+      fileStartDateSearchElement.value = "2021-08-30";
       fileStartDateSearchElement.dispatchEvent(new CustomEvent("change"));
       return Promise.resolve().then(() => {
         const dataTableElement = element.shadowRoot.querySelector(
           "lightning-datatable"
         );
-        expect(dataTableElement.data[0]).toStrictEqual(mockGetCaseRelatedFiles[0]);
+        expect(dataTableElement.data[0]).toStrictEqual(
+          mockGetCaseRelatedFiles[0]
+        );
       });
     });
   });
@@ -193,12 +210,128 @@ describe("c-idr-files-related-list suite", () => {
       const fileEndDateSearchElement = element.shadowRoot.querySelector(
         ".fileEndDateSearch"
       );
-      fileEndDateSearchElement.value = '2021-08-30';
+      fileEndDateSearchElement.value = "2021-08-30";
       fileEndDateSearchElement.dispatchEvent(new CustomEvent("change"));
       return Promise.resolve().then(() => {
         const dataTableElement = element.shadowRoot.querySelector(
           "lightning-datatable"
         );
+        expect(dataTableElement.data).toStrictEqual(mockGetCaseRelatedFiles);
+      });
+    });
+  });
+
+  it("search files from contains search", () => {
+    getCaseRelatedFiles.mockResolvedValue(mockGetCaseRelatedFiles);
+    searchFilesContent.mockResolvedValue(mockSearchFilesContent);
+    const element = createElement("c-idr-files-related-list", {
+      is: IdrFilesRelatedList
+    });
+    element.recordId = RECORD_ID;
+    document.body.appendChild(element);
+    return Promise.resolve().then(() => {
+      const fileContainsSearchElement = element.shadowRoot.querySelector(
+        ".fileContainsSearch"
+      );
+      fileContainsSearchElement.value = "Screen";
+      fileContainsSearchElement.dispatchEvent(new CustomEvent("change"));
+      return new Promise(setImmediate).then(() => {
+        const dataTableElement = element.shadowRoot.querySelector(
+          "lightning-datatable"
+        );
+        expect(dataTableElement.data).toStrictEqual(mockGetCaseRelatedFiles);
+      });
+    });
+  });
+
+  it("search files from contains search error occured", () => {
+    getCaseRelatedFiles.mockResolvedValue(mockGetCaseRelatedFiles);
+    searchFilesContent.mockRejectedValue(mockSearchFilesContentError);
+    const element = createElement("c-idr-files-related-list", {
+      is: IdrFilesRelatedList
+    });
+    element.recordId = RECORD_ID;
+    document.body.appendChild(element);
+
+    return Promise.resolve().then(() => {
+      const fileContainsSearchElement = element.shadowRoot.querySelector(
+        ".fileContainsSearch"
+      );
+      fileContainsSearchElement.value = "Screen";
+      fileContainsSearchElement.dispatchEvent(new CustomEvent("change"));
+      const handler = jest.fn();
+      element.addEventListener(ShowToastEventName, handler);
+      return new Promise(setImmediate).then(() => {
+        expect(handler).toHaveBeenCalled();
+        expect(handler.mock.calls[0][0].detail.message).toBe(
+          mockSearchFilesContentError.body.message
+        );
+      });
+    });
+  });
+
+  it("render files related to case on file upload finished", () => {
+    getCaseRelatedFiles.mockResolvedValue(mockGetCaseRelatedFiles);
+    const element = createElement("c-idr-files-related-list", {
+      is: IdrFilesRelatedList
+    });
+    element.recordId = RECORD_ID;
+    document.body.appendChild(element);
+    return new Promise(setImmediate).then(() => {
+      const fileUploadElement = element.shadowRoot.querySelector(
+        "c-file-Upload"
+      );
+      fileUploadElement.dispatchEvent(new CustomEvent("newfileupload"));
+      return new Promise(setImmediate).then(() => {
+        const dataTableElement = element.shadowRoot.querySelector(
+          "lightning-datatable"
+        );
+        expect(dataTableElement.data).toStrictEqual(mockGetCaseRelatedFiles);
+      });
+    });
+  });
+
+  it("error occured when render files related to case on file upload finished", () => {
+    getCaseRelatedFiles.mockRejectedValue(mockGetCaseRelatedFilesError);
+    const element = createElement("c-idr-files-related-list", {
+      is: IdrFilesRelatedList
+    });
+    element.recordId = RECORD_ID;
+    document.body.appendChild(element);
+    return new Promise(setImmediate).then(() => {
+      const fileUploadElement = element.shadowRoot.querySelector(
+        "c-file-Upload"
+      );
+      fileUploadElement.dispatchEvent(new CustomEvent("newfileupload"));
+      const handler = jest.fn();
+      element.addEventListener(ShowToastEventName, handler);
+      return new Promise(setImmediate).then(() => {
+        expect(handler).toHaveBeenCalled();
+        expect(handler.mock.calls[0][0].detail.message).toBe(
+          mockSearchFilesContentError.body.message
+        );
+      });
+    });
+  });
+
+  it("sorting data check", () => {
+    getCaseRelatedFiles.mockResolvedValue(mockGetCaseRelatedFiles);
+    const element = createElement("c-idr-files-related-list", {
+      is: IdrFilesRelatedList
+    });
+    element.recordId = RECORD_ID;
+    document.body.appendChild(element);
+    return new Promise(setImmediate).then(() => {
+      expect(element.files).not.toBe(null);
+      const dataTableElement = element.shadowRoot.querySelector(
+        "lightning-datatable"
+      );
+      dataTableElement.dispatchEvent(
+        new CustomEvent("sort", {
+          detail: { fieldName: "fileUrl", sortDirection: "asc" }
+        })
+      );
+      return Promise.resolve().then(() => {
         expect(dataTableElement.data).toStrictEqual(mockGetCaseRelatedFiles);
       });
     });
