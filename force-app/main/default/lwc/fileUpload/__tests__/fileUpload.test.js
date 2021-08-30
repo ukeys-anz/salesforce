d44d73e791caa6dbc8895e8f332ab89d9b98459b
mohama20 <adnanuddin.mohammed@anz.com>
2021-08-30 23:22:48 +1000

    Updated Jest test for file upload

--- force-app/main/default/lwc/fileUpload/__tests__/fileUpload.test.js
+++ force-app/main/default/lwc/fileUpload/__tests__/fileUpload.test.js
@@ -1,7 +1,9 @@
 import { createElement } from "lwc";
 import FileUpload from "c/fileUpload";
+import { ShowToastEventName } from "lightning/platformShowToastEvent";
 
 const RECORD_ID = "a0c2O00000197sUQAD";
+const TOAST_TITLE = "Success";
 
 // mock file test harness
 describe("Mock file for file upload testing", function () {
@@ -31,5 +33,31 @@ describe("Mock file for file upload testing", function () {
   it("Component should have record id", () => {
     const element = document.querySelector("c-file-upload");
     expect(element.recordId).toBe(RECORD_ID);
+  });
+
+  it("Test on file upload finished", () => {
+    const element = document.querySelector("c-file-upload");
+    const fileUploadElement = element.shadowRoot.querySelector(
+      "lightning-file-upload"
+    );
+    const handler = jest.fn();
+    element.addEventListener(ShowToastEventName, handler);
+    fileUploadElement.dispatchEvent(
+      new CustomEvent("uploadfinished", {
+        detail: {
+          files: [
+            {
+              name: "Screen Shot 2021-08-30 at 11.54.32 am.png",
+              documentId: "0692O000000hQ6hQAE",
+              contentVersionId: "0682O000000hmhuQAA"
+            }
+          ]
+        }
+      })
+    );
+    return Promise.resolve().then(() => {
+      expect(handler).toHaveBeenCalled();
+      expect(handler.mock.calls[0][0].detail.title).toBe(TOAST_TITLE);
+    });
   });
 });
