6bc6cbc77faef342919ff12d219b80202247da94
mohama20 <adnanuddin.mohammed@anz.com>
2021-09-09 10:39:55 +1000

    Renaming CMOSAPIRepository to IDRAPIRepository

--- force-app/main/default/lwc/customerInformation/customerInformation.js
+++ force-app/main/default/lwc/customerInformation/customerInformation.js
@@ -1,5 +1,5 @@
 import { LightningElement, api, wire, track } from "lwc";
-import getCustomerData from "@salesforce/apex/CMOSAPIRepository.getCustomerInfoLWC";
+import getCustomerData from "@salesforce/apex/IDRAPIRepository.getCustomerInfoLWC";
 import { getRecord, getFieldValue } from "lightning/uiRecordApi";
 import { updateRecord } from "lightning/uiRecordApi";
 import { refreshApex } from "@salesforce/apex";
