07d20ddfa74bd19563bbee2a636bf0f30103a093
lysei <85336134+rebeccajan@users.noreply.github.com>
2021-10-13 09:56:11 +1100

    ANZX-5910 Remove re-initiate button for QA (#2098)
    * hide re-initiate for QA
    
    * pmd update
    
    * Update
    
    * update to var name
    
    * update
    
    * update
    
    * Update as per cmts
    
    * update
    
    * update
    
    * Added new tests for user role
    
    Co-authored-by: Peter Charalambous <peter.charalambous@anz.com>

--- force-app/main/default/lwc/chatTopicRelatedList/__tests__/chatTopicRelatedList.test.js
+++ force-app/main/default/lwc/chatTopicRelatedList/__tests__/chatTopicRelatedList.test.js
@@ -34,6 +34,11 @@ const getRecordAdapter = registerLdsTestWireAdapter(getRecord);
 const MOCK_ACCOUNT_RECORD = require("./data/getAccountRecord.json");
 const MOCK_CASE_RECORD = require("./data/getCaseRecord.json");
 
+//This record also includes some account data as we are unable
+//to make multiple mock wire calls in one test
+const MOCK_QA_USER_RECORD = require("./data/getQAUserRecord.json");
+const MOCK_COACH_USER_RECORD = require("./data/getCoachUserRecord.json");
+
 describe("c-chat-topic-related-list", () => {
   afterEach(() => {
     // The jsdom instance is shared across test cases in a single file so reset the DOM
@@ -62,7 +67,6 @@ describe("c-chat-topic-related-list", () => {
       let header = element.shadowRoot.querySelector(
         "span[data-id='component-header']"
       );
-
       expect(header.textContent).toBe("Chat Topics (0)");
     });
   });
@@ -132,6 +136,44 @@ describe("c-chat-topic-related-list", () => {
 
       expect(header.textContent).toBe("Chat Topics (1)");
       expect(topicName.textContent).toBe("April2021");
+    });
+  });
+
+  it("tests re-initiate is visible", () => {
+    getChatTopicsOnAccount.mockResolvedValue(CHAT_TOPIC_MULTIPLE);
+    const element = createElement("c-chat-topic-related-list", {
+      is: ChatTopicRelatedList
+    });
+
+    element.objectName = "Account";
+    element.recordId = "0031700000pHcf8AAC";
+    getRecordAdapter.emit(MOCK_COACH_USER_RECORD);
+    document.body.appendChild(element);
+
+    return flushPromises().then(() => {
+      let reInitiateButton = element.shadowRoot.querySelector(
+        "lightning-menu-item[data-id='re_initiate']"
+      );
+      expect(reInitiateButton).toBeTruthy();
+    });
+  });
+
+  it("tests re-initiate is hidden for QA", () => {
+    getChatTopicsOnAccount.mockResolvedValue(CHAT_TOPIC_MULTIPLE);
+    const element = createElement("c-chat-topic-related-list", {
+      is: ChatTopicRelatedList
+    });
+
+    element.objectName = "Account";
+    element.recordId = "0031700000pHcf8AAC";
+    getRecordAdapter.emit(MOCK_QA_USER_RECORD);
+    document.body.appendChild(element);
+
+    return flushPromises().then(() => {
+      let reInitiateButton = element.shadowRoot.querySelector(
+        "lightning-menu-item[data-id='re_initiate']"
+      );
+      expect(reInitiateButton).toBeFalsy();
     });
   });
 });
