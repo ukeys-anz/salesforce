9530f29ec37b01fc821821e26a96fdd1cfb0ee7c
Jasmine Nguyen <ngockieutrang.nguyen@anz.com>
2021-10-25 15:12:20 +1100

    Fix lint error

--- force-app/main/default/lwc/createComplaintLWC/createComplaintLWC.js
+++ force-app/main/default/lwc/createComplaintLWC/createComplaintLWC.js
@@ -413,11 +413,6 @@ export default class CreateComplaintLWC extends NavigationMixin(
       event.detail.checked;
   }
 
-  handleIsDetailsProvidedToProductManufacturer(event) {
-    this.thirdPartyIsDetailsProvidedToProductManufacturer =
-      event.detail.checked;
-  }
-
   handleFinancialCompensation(event) {
     this.financialCompensation = event.target.value;
   }
