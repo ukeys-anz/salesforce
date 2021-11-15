bb1f23b1d67984bf469e393ad0554bea9e8e5417
FaridMasjediANZ <88009478+FaridMasjediANZ@users.noreply.github.com>
2021-11-16 09:09:17 +1100

    [ANZX-0000] Merge epic/fraud-lock-card into develop (#2387)

---
+++ force-app/main/default/lwc/viewCards/helper/helper-errors.js
@@ -0,0 +1,52 @@
+const ERROR_MESSAGES = {
+  invalidCard:
+    "Card Information is invalid. Please reach out to your system administrator.",
+  cardDetailsError:
+    "Failed to retrieve card list. Please refresh and try again. If the problem persists, please contact your System Administrator."
+};
+
+const toastTitles = {
+  subscription: "Card Management",
+  cardDetailsError: "Card List Load Failed"
+};
+
+function handleErrorJSON(error) {
+  try {
+    JSON.parse(error);
+  } catch (e) {
+    return error;
+  }
+  return JSON.parse(error).error;
+}
+
+function errorNotToBeAWord(error, whichError) {
+  if (error && error.message) {
+    let message = handleErrorJSON(error.message);
+    if (message && typeof message === "string") {
+      if (message.split(" ").length > 1) {
+        return message;
+      }
+    }
+  }
+  return whichError;
+}
+
+export const errorHanlder = {
+  invalidCard: ERROR_MESSAGES.invalidCard,
+
+  subscription: function (msg) {
+    return {
+      title: toastTitles.subscription,
+      message: msg.message,
+      variant: msg.success ? "success" : "error"
+    };
+  },
+
+  cardDetailsError: function (msg) {
+    return {
+      title: toastTitles.cardDetailsError,
+      message: errorNotToBeAWord(msg, ERROR_MESSAGES.cardDetailsError),
+      variant: "error"
+    };
+  }
+};
