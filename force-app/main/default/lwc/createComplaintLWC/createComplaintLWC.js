5a1e25c9326244482bcb3e5c770122b80884f084
mohama20 <82633116+mohama20@users.noreply.github.com>
2021-12-07 10:32:48 +1100

    IDR-3294-Express CMOS Case Creation (#2493)
    * Adding new Known Issues object
    
    * Adding Issue Type Field
    
    * Adding product lookup
    
    * Updating Issue type to use global value set
    
    * Added Sub Issue Type field to Known Issues
    
    * Adding Description of Issue field
    
    * Adding Customer Desired Outcome field
    
    * Updating API names for fields
    
    * Deleting the incorrect api name fields
    
    * Adding Is there another issue field
    
    * Adding Written response required field
    
    * Updating helptext to fields
    
    * Adding REAL for required field
    
    * Adding PSI field
    
    * Added Status field
    
    * Adding remaining fields
    
    * Adding Data Governance Categories to new fields
    
    * Adding Known Issues lookup field to Case object
    
    * Adding Metadata Object
    
    * Known Issue Tab, CompMgmtApp Upd and Profile
    
    * Updating hardship field label
    
    * Adding Written Response Requested field
    
    * Adding new express cmos complaint field
    
    * Adding express case controller
    
    * Adding controler meta file
    
    * Adding express complaint lwc component
    
    * Updating create complaint
    
    * Adding Manage Know Issue Permission Set
    
    * Read permission for IDR User for known issues
    
    * Fixing LINT
    
    * Updating Admin profile with corrrect Express CMOS
    
    * Fixing PMD for apex controller class
    
    * fixing eslint issues on lwc
    
    * fixing eslint issue on lwc component
    
    * Adding encryption tag to fields
    
    * Adding apex class access to idr user pset
    
    * Test class and its changes for controller
    
    * Adding Known Issue Records file to import
    
    * Disabling toggle after know issue is selected
    
    * Removing custom metadata ref as its not req
    
    * Adding toast message for error
    
    * Jest test for express cmos
    
    * Adding proper test class
    
    * Removing incorrect named test class
    
    * Fixing PMD issue on test class
    
    * Removing encryption for checkbox fields
    
    * rerun jobs
    
    * rerun checks
    
    * Removing encryption for validation
    
    * Updated test class
    
    * Updating test class for validation fix
    
    * Updating permission set for Known issue object acc
    
    * Making known issue read only on layouts
    
    * Status to include only few values
    
    * Preventing deletion on perm set for known issues
    
    * Formatting html part of lwc
    
    * Adding new fields to read only pset
    
    * Sharing rule and object private setting
    
    * Adding sharing rule to forceignore
    
    * Setting view all to false in pset
    
    * allowing delete for fixing perm set validation
    
    * Removing sharing rule from forceignore
    
    * Updating to role and subordinates
    
    * Temp test class fix for validation and deployment
    
    * updating test class
    
    Co-authored-by: Aidan Majewski <aidan.majewski@anz.com>
    Co-authored-by: Bala <62235340+duraisab@users.noreply.github.com>

--- force-app/main/default/lwc/createComplaintLWC/createComplaintLWC.js
+++ force-app/main/default/lwc/createComplaintLWC/createComplaintLWC.js
@@ -95,6 +95,10 @@ import SUBSEQUENT_ISSUE_TYPE_3 from "@salesforce/schema/Case.IDR_Subsequent_Issu
 import PRODUCT_LOOKUP_FIELD_3 from "@salesforce/schema/Case.IDR_Product_3__c";
 import ACCOUNT_POLICY_FIELD_3 from "@salesforce/schema/Case.IDR_Account_Card_Policy_Number_3__c";
 
+//Express CMOS fields
+import KNOWN_ISSUES from "@salesforce/schema/Case.IDR_Known_Issues__c";
+import EXPRESS_CMOS from "@salesforce/schema/Case.IDR_Express_CMOS_Complaint__c";
+
 const ERROR_REQUIRED_TITLE = "Please complete all required fields:\n";
 const ERROR_UNKNOWN_TITLE = "An error has occurred.";
 const SUCCESS = "success";
@@ -201,6 +205,10 @@ export default class CreateComplaintLWC extends NavigationMixin(
   subsequentIssue3 = SUBSEQUENT_ISSUE_TYPE_3;
   productValue3 = PRODUCT_LOOKUP_FIELD_3;
   accountOrPolicyNumber3 = "";
+
+  // Express CMOS fields
+  knownIssue = KNOWN_ISSUES;
+  expressCMOS = EXPRESS_CMOS;
 
   @api recordTypeId;
   @api recordTypeDevName;
@@ -791,6 +799,9 @@ export default class CreateComplaintLWC extends NavigationMixin(
         ] = this.accountOrPolicyNumber3;
       }
 
+      fields[KNOWN_ISSUES.fieldApiName] = this.knownIssue;
+      fields[EXPRESS_CMOS.fieldApiName] = this.expressCMOS;
+
       const recordInput = { apiName: CASE_OBJECT.objectApiName, fields };
       createRecord(recordInput)
         .then((response) => {
@@ -991,5 +1002,129 @@ export default class CreateComplaintLWC extends NavigationMixin(
   closeModal() {
     this.template.querySelector(".slds-hide").classList.remove("slds-hide");
     this.showModal = false;
+  }
+
+  expressToggleCheckedHandler() {
+    this.isComplaintResolved = true;
+    this.isNonFinancialComplaintRemedy = true;
+    const issueTypeElement = this.template.querySelector(
+      '[data-id="issueType-id"]'
+    );
+    issueTypeElement.dispatchEvent(new CustomEvent("change"));
+  }
+
+  knownIssueChangeHandler(event) {
+    const issueTypeElement = this.template.querySelector(
+      '[data-id="issueType-id"]'
+    );
+    issueTypeElement.value = event.detail.IDR_Issue_Type__c;
+    issueTypeElement.dispatchEvent(new CustomEvent("change"));
+
+    const subIssueTypeElement = this.template.querySelector(
+      '[data-id="subsequentIssue-id"]'
+    );
+    subIssueTypeElement.value = event.detail.IDR_Sub_Issue_Type__c;
+    const productElement = this.template.querySelector(
+      '[data-id="product-id"]'
+    );
+    productElement.value = event.detail.Product__c;
+
+    //Check needed for non customer complaint
+    if (this.isCustomerComplaint) {
+      const accountPolicyNoElement = this.template.querySelector(
+        '[data-id="accPolicyNum-id"]'
+      );
+      accountPolicyNoElement.handleExpressCase();
+    }
+    const descriptionElement = this.template.querySelector(
+      '[data-id="descOfIssue-id"]'
+    );
+    descriptionElement.value = event.detail.IDR_Description_of_Issue__c;
+    const custDesiredOutElement = this.template.querySelector(
+      '[data-id="custOutCome"]'
+    );
+    custDesiredOutElement.value = event.detail.IDR_Customer_Desired_Outcome__c;
+    const anotherIssueElement = this.template.querySelector(
+      '[data-id="issue2-id"]'
+    );
+    anotherIssueElement.checked = event.detail.IDR_Is_there_another_issue__c;
+    anotherIssueElement.dispatchEvent(new CustomEvent("change"));
+    const writtenRequestedElement = this.template.querySelector(
+      '[data-id="writtenResponseGroup-id"]'
+    );
+    writtenRequestedElement.value =
+      event.detail.IDR_Written_Response_Requested__c;
+    writtenRequestedElement.dispatchEvent(
+      new CustomEvent("change", {
+        detail: {
+          value: writtenRequestedElement.value
+        }
+      })
+    );
+
+    const writtenRespElement = this.template.querySelector(
+      '[data-id="writtenRequiredGroup-id"]'
+    );
+    writtenRespElement.value = event.detail.IDR_Written_Response_Required__c;
+    writtenRespElement.dispatchEvent(
+      new CustomEvent("change", {
+        detail: {
+          value: writtenRespElement.value
+        }
+      })
+    );
+
+    const realFormElement = this.template.querySelector(
+      '[data-id="realFormRequiredGroup-id"]'
+    );
+    if (event.detail.IDR_REAL_Form_Required__c) {
+      realFormElement.value = "Yes";
+      realFormElement.dispatchEvent(
+        new CustomEvent("change", {
+          detail: {
+            value: "Yes"
+          }
+        })
+      );
+    } else {
+      realFormElement.value = "No";
+      realFormElement.dispatchEvent(
+        new CustomEvent("change", {
+          detail: {
+            value: "No"
+          }
+        })
+      );
+    }
+
+    const possibleSysIssueElement = this.template.querySelector(
+      '[data-id="commoncomplaintGroup-id"]'
+    );
+    possibleSysIssueElement.value = event.detail.IDR_Possible_Systemic_Issue__c;
+    possibleSysIssueElement.dispatchEvent(new CustomEvent("change"));
+    const statusElement = this.template.querySelector(
+      '[data-id="caseStatus-id"]'
+    );
+    statusElement.value = event.detail.IDR_Status__c;
+    statusElement.dispatchEvent(new CustomEvent("change"));
+    const complaintOutcomeElement = this.template.querySelector(
+      '[data-id="compOutCome-id"]'
+    );
+    complaintOutcomeElement.value = event.detail.IDR_Complaint_Outcome__c;
+    const descOfOutcomeElement = this.template.querySelector(
+      '[data-id="descOutcome-id"]'
+    );
+    descOfOutcomeElement.value = event.detail.IDR_Description_of_Outcome__c;
+    const complaintRemedyElement = this.template.querySelector(
+      '[data-id="compRemedy-id"]'
+    );
+    complaintRemedyElement.value = event.detail.IDR_Complaint_Remedy__c;
+    const nonFinancialRemedyElement = this.template.querySelector(
+      '[data-id="nonFinancialRemedy-id"]'
+    );
+    nonFinancialRemedyElement.value = event.detail.IDR_Non_Financial_Remedy__c;
+
+    this.knownIssue = event.detail.Id;
+    this.expressCMOS = true;
   }
 }
