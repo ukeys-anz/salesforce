9415734a93ddb7c7d9b2855c16cb5f7a6532ebc0
Peter Charalambous <62224246+PeterCharalambousANZ@users.noreply.github.com>
2021-12-20 11:31:28 +1100

    ANZX-29189 accounts api callout (#2630)
    * Created financial account model
    
    * Updated model to snake case
    
    * Updated model and added mock
    
    * Added apex logic for financial account callout
    
    * Refactored LWC logic for new apex callout
    
    * Updated apex tests for financial accounts
    
    * Removed update financial accounts class
    
    * Added sanity check for required fields
    
    * Removed console log, updated ternary operatory
    
    * Updated model for PMD
    
    * Updated financial account controller logic
    
    * Updated lwc to reflect apex changes
    
    * Updated mocks
    
    * Updated suppression for model
    
    * ES lint changes
    
    * Commented out accounts and goals refresh as it needs full refactor
    
    * Added placeholder jest test
    
    * Commented accounts goals refresh component
    
    * es lint changes
    
    * removed class reference from jest test
    
    * removed refresh component from page layout
    
    * rerun checks
    
    * fixing deployment error with forceignore
    
    * fixing broken forceignore
    
    * rerun checks
    
    * capitalise test vars
    
    Co-authored-by: Aidan Majewski <aidan.majewski@anz.com>
    Co-authored-by: Bec <Rebecca.Jan@anz.com>

--- force-app/main/default/lwc/personAccountFinancialDetails/personAccountFinancialDetails.js
+++ force-app/main/default/lwc/personAccountFinancialDetails/personAccountFinancialDetails.js
@@ -3,14 +3,11 @@ import { LightningElement, api, wire } from "lwc";
 import { getRecord } from "lightning/uiRecordApi";
 import { handleErrorShowToast } from "c/utils";
 
-/* IMPORT API METHODS*/
-import getAccounts from "@salesforce/apex/CoachBankingAPIRepository.getAccountsAura";
-
 /* IMPORT APEX METHODS */
 import getTotalBalance from "@salesforce/apex/TotalBalanceController.getTotalBalance";
 import getTotalSaved from "@salesforce/apex/TotalBalanceController.getTotalSaved";
-import updateAccounts from "@salesforce/apex/UpdateFinancialAccounts.updateAccounts";
-import getFinancialAccounts from "@salesforce/apex/FinancialAccountController.getFinancialAccounts";
+import getFinancialAccountFabric from "@salesforce/apex/FinancialAccountController.getFinancialAccountFabric";
+import getFinancialAccountDB from "@salesforce/apex/FinancialAccountController.getFinancialAccountDB";
 
 /* IMPORT PERMISSIONS */
 import hasAccountsGoalsPermission from "@salesforce/customPermission/ANZx_Accounts_and_Goals";
@@ -21,14 +18,9 @@ import ACCOUNT_OCV_ID_FIELD from "@salesforce/schema/Account.OCV_ID__c";
 export default class PersonAccountFinancialDetails extends LightningElement {
   @api recordId;
   @api objectName;
-  accountDetails = [];
   goalDetails = [];
-  accountNumbers = [];
-  goalAccountNumbers = [];
   ocvId;
   loading;
-  checkingError;
-  savingsError;
   goalError;
   totalBalanceError;
   accountData = {
@@ -56,134 +48,17 @@ export default class PersonAccountFinancialDetails extends LightningElement {
   async getFinancialData() {
     //Reset values to avoid duplicates
     this.loading = true;
-    this.accountDetails = [];
     this.goalDetails = [];
-    this.accountNumbers = [];
-    this.goalAccountNumbers = [];
     this.accountData = {
       checking: [],
       savings: []
     };
-
     try {
-      //Get the latest account details and update them
-      let { accountList } = await getAccounts({
+      //Attempt to get the latest account details from fabric
+      let accountDetails = await getFinancialAccountFabric({
         ocvId: this.ocvId
       });
-      if (accountList) {
-        accountList.forEach((account) => {
-          // check to drop or show an error message against a particular Fin Account record based on response
-          if (account.isValid) {
-            this.accountNumbers.push(account.accountNumber);
-            let accountInformation = {
-              Name: account.name,
-              FinServ__FinancialAccountNumber__c: account.accountNumber,
-              FinServ__Balance__c: account.balance.value,
-              FinServ__CurrentPostedBalance__c: account.currentBalance.value,
-              BSB__c: account.bsb.toString(),
-              FinServ__OpenDate__c: account.openDate
-            };
-
-            this.accountDetails.push(accountInformation);
-            if (account.accountType === "Savings") {
-              this.goalAccountNumbers.push(account.accountNumber);
-              let goalInformation = {
-                name: account.goal.name,
-                accountNumber: account.accountNumber,
-                targetAmount: account.goal.targetAmount
-                  ? account.goal.targetAmount.value
-                  : "",
-                currentBalance: account.currentBalance.value,
-                startDate: account.goal.startDate,
-                targetDate: account.goal.targetDate
-                  ? account.goal.targetDate
-                  : "",
-                icon: account.goal.iconId
-              };
-              this.goalDetails.push(goalInformation);
-            }
-          }
-        });
-
-        try {
-          //Update accounts
-          await updateAccounts({
-            ownerId: this.recordId,
-            accountNumbers: this.accountNumbers,
-            financialAccounts: this.accountDetails
-          });
-        } catch (error) {
-          handleErrorShowToast(
-            this,
-            "Failed To Update Account Details",
-            error,
-            "Failed to update account details. Please refresh and try again. If issue persists please contact your System Administrator.",
-            "pester"
-          );
-        } finally {
-          try {
-            let { totalBalance } = await getTotalBalance({
-              ownerId: this.recordId
-            });
-            this.totalBalance = totalBalance;
-
-            let { totalSaved } = await getTotalSaved({
-              ownerId: this.recordId
-            });
-            this.totalSaved = totalSaved;
-          } catch (error) {
-            this.totalBalanceError =
-              "Failed to retrieve total balance details. Please refresh and try again. If issue persists please contact your System Administrator";
-            handleErrorShowToast(
-              this,
-              "Failed To Retrieve Total Balance Details",
-              error,
-              this.totalBalanceError,
-              "pester"
-            );
-          }
-
-          try {
-            let checkingAccount = await getFinancialAccounts({
-              ownerId: this.recordId,
-              recordLimit: 1,
-              type: "checking"
-            });
-            checkingAccount = this.handleAccountBadgeClass(checkingAccount);
-            this.accountData.checking.push(checkingAccount[0]);
-          } catch (error) {
-            this.checkingError =
-              "Failed to retrieve checking account details. Please refresh and try again. If issue persists please contact your System Administrator";
-            handleErrorShowToast(
-              this,
-              "Failed To Retrieve Checking Account Details",
-              error,
-              this.checkingError,
-              "pester"
-            );
-          }
-
-          try {
-            let savingsAccount = await getFinancialAccounts({
-              ownerId: this.recordId,
-              recordLimit: 1,
-              type: "savings"
-            });
-            savingsAccount = this.handleAccountBadgeClass(savingsAccount);
-            this.accountData.savings.push(savingsAccount[0]);
-          } catch (error) {
-            this.savingsError =
-              "Failed to retrieve savings account details. Please refresh and try again. If issue persists please contact your System Administrator";
-            handleErrorShowToast(
-              this,
-              "Failed To Retrieve Savings Account Details",
-              error,
-              this.savingsError,
-              "pester"
-            );
-          }
-        }
-      }
+      this.handleAccountInformation(accountDetails);
     } catch (error) {
       handleErrorShowToast(
         this,
@@ -192,18 +67,55 @@ export default class PersonAccountFinancialDetails extends LightningElement {
         "Failed to retrieve latest account details. Please refresh and try again. If issue persists please contact your System Administrator",
         "pester"
       );
+
+      //If the API callout fails to fetch latest data, use this as a fallback to fetch
+      //the records stored in Salesforce
+      let accountDetails = await getFinancialAccountDB({
+        ownerId: this.recordId,
+        type: ["checking", "savings"]
+      });
+      this.handleAccountInformation(accountDetails);
     } finally {
+      try {
+        let { totalBalance } = await getTotalBalance({
+          ownerId: this.recordId
+        });
+        this.totalBalance = totalBalance;
+
+        let { totalSaved } = await getTotalSaved({
+          ownerId: this.recordId
+        });
+        this.totalSaved = totalSaved;
+      } catch (error) {
+        this.totalBalanceError =
+          "Failed to retrieve total balance details. Please refresh and try again. If issue persists please contact your System Administrator";
+        handleErrorShowToast(
+          this,
+          "Failed To Retrieve Total Balance Details",
+          error,
+          this.totalBalanceError,
+          "pester"
+        );
+      }
+
       this.loading = false;
     }
   }
 
-  handleAccountBadgeClass(finAccounts) {
+  handleAccountInformation(finAccounts) {
     finAccounts.forEach((account) => {
       //Set the badge class based on the status
-      if (account.FinServ__Status__c === "Active") {
-        account.badgeClass = "slds-badge slds-theme_success";
+      account.badgeClass =
+        account.FinServ__Status__c === "Active"
+          ? "slds-badge slds-theme_success"
+          : "slds-badge slds-theme_error";
+      //Determine the type of financial account
+      if (
+        account.FinServ__FinancialAccountType__c.toLowerCase() === "checking"
+      ) {
+        this.accountData.checking.push(account);
       } else {
-        account.badgeClass = "slds-badge slds-theme_error";
+        this.accountData.savings.push(account);
       }
     });
 
