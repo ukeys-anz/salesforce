07ecaa5d71114e76b5743e8cdaec5e3804dda230
Farid Masjedi <farid.masjedi@anz.com>
2022-02-03 20:25:17 +1100

    Update snapshotScratch.sh | Update Post Deploy step

--- bash-scripts/snapshotScratch.sh
+++ bash-scripts/snapshotScratch.sh
@@ -115,16 +115,19 @@ echoMessageCreator "" $stepNo false
 
 # post deploy: to make all the files back to what it was and deploy them
 echoMessageCreator "Post Deploy" $stepNo true
-f=force-app/main/default/objects/Case/fields/IDR_Restriction_Level__c.field-meta.xml
-cp IDRRestriction.txt $f    
-sfdx force:source:deploy -p $f
-
-f=force-app/main/default/objects/Case/fields/SI_Workflow_Step__c.field-meta.xml    
-cp SIWorkflow.txt $f  
-sfdx force:source:deploy -p $f
-
-rm -rf IDRRestriction.txt
-rm -rf SIWorkflow.txt
+changeMetadata force-app/main/default/objects/Case/fields/IDR_Restriction_Level__c.field-meta.xml IDRRestriction false
+changeMetadata force-app/main/default/objects/Case/fields/SI_Workflow_Step__c.field-meta.xml SIWorkflow false
+changeMetadata force-app/main/default/permissionsets/Mvision_Permissions.permissionset-meta.xml Mvision false
+changeMetadata force-app/main/default/permissionsets/Read_Only_Admin.permissionset-meta.xml readOnly false
+changeMetadata force-app/main/default/permissionsets/SFDX_Deploy.permissionset-meta.xml sfdxDeploy false
+changeMetadata force-app/main/default/permissionsets/SFDX_Snapshots.permissionset-meta.xml sfdxSnap false
+changeMetadata force-app/main/default/permissionsets/View_All_Data.permissionset-meta.xml viewAll false
+changeMetadata force-app/main/default/permissionsets/View_All_Files.permissionset-meta.xml viewFiles false
+changeMetadata "force-app/main/default/profiles/Minimum Access - External Apps.profile-meta.xml" minimum false
+changeMetadata "force-app/main/default/profiles/ANZx Standard User.profile-meta.xml" anzxStandard false
+f1=force-app/main/default/objects/Case/fields/IDR_Restriction_Level__c.field-meta.xml
+f2=force-app/main/default/objects/Case/fields/SI_Workflow_Step__c.field-meta.xml
+sfdx force:source:deploy -u $scratchorgalias -p $f1,$f2
 echoMessageCreator "" $stepNo false
 ###########################
 
