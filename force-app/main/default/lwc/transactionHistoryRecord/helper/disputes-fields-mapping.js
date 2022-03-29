bac0c1a747278eeb37fb3bb3a5ebdc75bac93aeb
lanceyshianz <62575151+lanceyshianz@users.noreply.github.com>
2022-03-29 15:53:43 +1100

    ANZX-8153 pending dispute queues and assignment rules (#3133)
    * Add two new queues
    
    * Rename the queues to be card queues
    
    * Update queue label
    
    * Add transaction type field and add into record types
    
    * Add picklist values for all record types
    
    * Add mapping for js file
    
    * Add pending assignment rules
    
    * Add unit test
    
    * Add validation rule and layout info
    
    * Add permission set info
    
    * Add panther info
    
    * Fix pmd issues
    
    * Fix pmd issue
    
    * Use faker to generate random string for test
    
    * Fix things mentioned by Sujan
    
    * Remove picklist values in general inquiry
    
    * Add pending queues into validation error message
    
    * Fix compile issues
    
    * Add return value for pmd
    
    * Fix validation error message since it exceeds wording limit

--- force-app/main/default/lwc/transactionHistoryRecord/helper/disputes-fields-mapping.js
+++ force-app/main/default/lwc/transactionHistoryRecord/helper/disputes-fields-mapping.js
@@ -41,7 +41,8 @@ const commonFieldsMapping = {
   Date_Posted__c: "transaction_posted_date",
   Transaction_Id__c: "origination_id",
   Transaction_UUID__c: "transaction_id",
-  Transaction_Type__c: "type"
+  Transaction_Type__c: "type",
+  Transaction_Status__c: "status"
 }; // Common fields between all dispute types from the payload
 
 // Pre-populate fields that are passed individually from the lwc, which we cannot loop through
