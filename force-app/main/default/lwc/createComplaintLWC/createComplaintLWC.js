04a43243e9252c8b7d6c5d90ce984940ef4eb2da
Hima-Anz <67941446+Hima-Anz@users.noreply.github.com>
2022-06-06 10:33:58 +1000

    AR-13274 bug fix (#3800)

--- force-app/main/default/lwc/createComplaintLWC/createComplaintLWC.js
+++ force-app/main/default/lwc/createComplaintLWC/createComplaintLWC.js
@@ -1443,6 +1443,7 @@ export default class CreateComplaintLWC extends NavigationMixin(
       (this.closeFields[REMEDY_POINTS1.fieldApiName].includes(dotCheck) ||
         this.closeFields[REMEDY_POINTS1.fieldApiName] < 0)
     ) {
+      validToSave = false;
       this.missingDataFields +=
         "Financial Remedy Points 1 must be a positive value without decimals";
     }
