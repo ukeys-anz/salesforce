908bfdb17b4516ec4724c9da50484b467cadf1ba
Kopal <kopal.goswami@anz.com>
2023-05-23 17:35:48 +0530

    ANZX-95731 create a case from a Store Interaction

---
+++ force-app/main/default/lwc/createCaseButton/createCaseButton.js
@@ -0,0 +1,21 @@
+import { LightningElement, api } from "lwc";
+import { NavigationMixin } from 'lightning/navigation';
+
+
+export default class CreateCaseButton extends NavigationMixin(LightningElement) {
+    isExecuting = false;
+    @api invoke() {
+        if (this.isExecuting) {
+            return;
+        }
+        this.isExecuting = true;
+        this[NavigationMixin.Navigate]({
+            type: 'standard__objectPage',
+            attributes: {
+                objectApiName: 'Case',
+                actionName: 'new',
+            },
+        });
+        this.isExecuting = false;
+    }
+}
