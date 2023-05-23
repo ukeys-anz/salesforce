b1a4a71872e1a86f8021cf8f056db9bb6872d47c
Kopal <kopal.goswami@anz.com>
2023-05-23 19:17:02 +0530

    ANZX-95731: create a case from a Store Interaction record

--- force-app/main/default/lwc/createCaseButton/createCaseButton.js
+++ force-app/main/default/lwc/createCaseButton/createCaseButton.js
@@ -1,21 +1,22 @@
 import { LightningElement, api } from "lwc";
-import { NavigationMixin } from 'lightning/navigation';
+import { NavigationMixin } from "lightning/navigation";
 
-
-export default class CreateCaseButton extends NavigationMixin(LightningElement) {
-    isExecuting = false;
-    @api invoke() {
-        if (this.isExecuting) {
-            return;
-        }
-        this.isExecuting = true;
-        this[NavigationMixin.Navigate]({
-            type: 'standard__objectPage',
-            attributes: {
-                objectApiName: 'Case',
-                actionName: 'new',
-            },
-        });
-        this.isExecuting = false;
+export default class CreateCaseButton extends NavigationMixin(
+  LightningElement
+) {
+  isExecuting = false;
+  @api invoke() {
+    if (this.isExecuting) {
+      return;
     }
+    this.isExecuting = true;
+    this[NavigationMixin.Navigate]({
+      type: "standard__objectPage",
+      attributes: {
+        objectApiName: "Case",
+        actionName: "new"
+      }
+    });
+    this.isExecuting = false;
+  }
 }
