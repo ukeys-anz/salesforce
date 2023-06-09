b4f72769e6cbfb43094702fdaaf12e34226a2240
mohama20 <82633116+mohama20@users.noreply.github.com>
2023-06-09 11:51:05 +1000

    Updated js for block field validation (#6699)

--- force-app/main/default/lwc/createCaseOmni/createCaseOmni.js
+++ force-app/main/default/lwc/createCaseOmni/createCaseOmni.js
@@ -150,7 +150,12 @@ export default class CreateCaseOmni extends OmniscriptBaseMixin(
 
   checkFields(detail, reMap) {
     reMap.forEach((field) => {
-      if (!detail[Object.keys(field)[0]]) {
+      let ele = Object.keys(field)[0];
+      if (ele.includes("Block")) {
+        if (!detail[ele] || (detail[ele] && !detail[ele][ele.split("-")[0]])) {
+          this.missingFields.push(Object.values(field)[0]);
+        }
+      } else if (!detail[ele]) {
         this.missingFields.push(Object.values(field)[0]);
       }
     });
