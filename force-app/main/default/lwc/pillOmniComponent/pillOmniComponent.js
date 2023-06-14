807610f674d31430888008258387f969a3a4b8fe
kalaln <Nilesh.Kalal@anz.com>
2023-06-14 11:50:19 +0530

    [ANZX-99370] Setup only LWC's, Classes used in Omni

---
+++ force-app/main/default/lwc/pillOmniComponent/pillOmniComponent.js
@@ -0,0 +1,95 @@
+/**
+ * Created by Nagendra on 02-06-2023.
+ */
+
+import { api, LightningElement } from "lwc";
+import { OmniscriptBaseMixin } from "omnistudio/omniscriptBaseMixin";
+
+export default class PillOmniComponent extends OmniscriptBaseMixin(
+  LightningElement
+) {
+  @api allValues;
+  @api elementName;
+
+  handleRemoveTags(event) {
+    const valueToRemove = event.target.name;
+    console.log("this.valueToRemove 2-> " + JSON.stringify(valueToRemove));
+    console.log("before this.allValues-> " + JSON.stringify(this.allValues));
+    // eslint-disable-next-line @lwc/lwc/no-api-reassignments
+    this.allValues = this.deselectValue(this.allValues, valueToRemove);
+    //this.allValues = this.allValues.filter(item => item.value === valueToRemove);
+
+    console.log("this.allValues-> " + JSON.stringify(this.allValues));
+    if (
+      this.omniJsonData &&
+      this.omniJsonData.basenode &&
+      this.elementName === "CustomLWC2"
+    ) {
+      //let basenodeList = this.omniJsonData.basenode;
+      //let newBase = this.handleRemoveMatching(basenodeList, this.allValues);
+
+      let makeBaseNodeBlank = {
+        basenode: this.allValues
+      };
+      this.omniApplyCallResp(makeBaseNodeBlank);
+    }
+
+    if (
+      this.omniJsonData &&
+      this.omniJsonData.basenodeForReasonForCV &&
+      this.elementName === "CustomLWC3"
+    ) {
+      //let basenodeList = this.omniJsonData.basenodeForReasonForCV;
+      //let newBase = this.handleRemoveMatching(basenodeList, this.allValues);
+
+      let makeBaseNodeBlank = {
+        basenodeForReasonForCV: this.allValues
+      };
+      this.omniApplyCallResp(makeBaseNodeBlank);
+    }
+    if (
+      this.omniJsonData &&
+      this.omniJsonData.productbasenode &&
+      this.elementName === "CustomLWC7"
+    ) {
+      //let basenodeList = this.omniJsonData.productbasenode;
+      //let newBase = this.handleRemoveMatching(basenodeList, this.allValues);
+
+      let makeBaseNodeBlank = {
+        productbasenode: this.allValues
+      };
+      this.omniApplyCallResp(makeBaseNodeBlank);
+    }
+  }
+
+  deselectValue(listval, val) {
+    let lstValues = [];
+    for (let eachItem of listval) {
+      if (eachItem.value === val) {
+        lstValues.push({
+          value: eachItem.value,
+          label: eachItem.label,
+          selected: false
+        });
+      } else {
+        lstValues.push({
+          value: eachItem.value,
+          label: eachItem.label,
+          selected: eachItem.selected
+        });
+      }
+    }
+    return lstValues;
+  }
+
+  handleRemoveMatching(list1, list2) {
+    if (list2.length === 0) {
+      return [];
+    }
+    const list2Values = list2.map((item) => item.value);
+
+    list1 = list1.filter((item) => !list2Values.includes(item.value));
+
+    return list1;
+  }
+}
