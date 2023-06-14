f784368a0d12a5331b42dcaec2d6bf2e0adfd648
singhn60 <Nagendra.Singh@anz.com>
2023-06-14 14:55:06 +0530

    [ANZX-99370] IP and DRs for Interaction create and edit flow

--- force-app/main/default/lwc/pillOmniComponent/pillOmniComponent.js
+++ force-app/main/default/lwc/pillOmniComponent/pillOmniComponent.js
@@ -2,30 +2,36 @@
  * Created by Nagendra on 02-06-2023.
  */
 
-import { api, LightningElement } from "lwc";
+import { api, track, LightningElement } from "lwc";
 import { OmniscriptBaseMixin } from "omnistudio/omniscriptBaseMixin";
 
 export default class PillOmniComponent extends OmniscriptBaseMixin(
   LightningElement
 ) {
-  @api allValues;
+  @track _allValues;
   @api elementName;
+
+  @api
+  get allValues() {
+    return this._allValues;
+  }
+
+  set allValues(value) {
+    console.log("value -> " + JSON.stringify(value));
+    this._allValues = value;
+  }
 
   handleRemoveTags(event) {
     const valueToRemove = event.target.name;
-    // eslint-disable-next-line @lwc/lwc/no-api-reassignments
-    this.allValues = this.deselectValue(this.allValues, valueToRemove);
-    //this.allValues = this.allValues.filter(item => item.value === valueToRemove);
+    this._allValues = this.deselectValue(this._allValues, valueToRemove);
+
     if (
       this.omniJsonData &&
       this.omniJsonData.basenode &&
-      this.elementName === "CustomLWC2"
+      this.elementName === "pillForReason"
     ) {
-      //let basenodeList = this.omniJsonData.basenode;
-      //let newBase = this.handleRemoveMatching(basenodeList, this.allValues);
-
       let makeBaseNodeBlank = {
-        basenode: this.allValues
+        basenode: this._allValues
       };
       this.omniApplyCallResp(makeBaseNodeBlank);
     }
@@ -33,26 +39,20 @@ export default class PillOmniComponent extends OmniscriptBaseMixin(
     if (
       this.omniJsonData &&
       this.omniJsonData.basenodeForReasonForCV &&
-      this.elementName === "CustomLWC3"
+      this.elementName === "pillForTopic"
     ) {
-      //let basenodeList = this.omniJsonData.basenodeForReasonForCV;
-      //let newBase = this.handleRemoveMatching(basenodeList, this.allValues);
-
       let makeBaseNodeBlank = {
-        basenodeForReasonForCV: this.allValues
+        basenodeForReasonForCV: this._allValues
       };
       this.omniApplyCallResp(makeBaseNodeBlank);
     }
     if (
       this.omniJsonData &&
       this.omniJsonData.productbasenode &&
-      this.elementName === "CustomLWC7"
+      this.elementName === "pillForProduct"
     ) {
-      //let basenodeList = this.omniJsonData.productbasenode;
-      //let newBase = this.handleRemoveMatching(basenodeList, this.allValues);
-
       let makeBaseNodeBlank = {
-        productbasenode: this.allValues
+        productbasenode: this._allValues
       };
       this.omniApplyCallResp(makeBaseNodeBlank);
     }
@@ -76,16 +76,5 @@ export default class PillOmniComponent extends OmniscriptBaseMixin(
       }
     }
     return lstValues;
-  }
-
-  handleRemoveMatching(list1, list2) {
-    if (list2.length === 0) {
-      return [];
-    }
-    const list2Values = list2.map((item) => item.value);
-
-    list1 = list1.filter((item) => !list2Values.includes(item.value));
-
-    return list1;
   }
 }
