ce7a78df7636641481ece8eddb3f45d003b4ff3d
mohama20 <adnanuddin.mohammed@anz.com>
2023-06-27 11:19:32 +1000

    Revert "CC-4169 Non-customer case creation (#6861)"
    This reverts commit 818cc747183bc8e85db26e60a8a56fcdc1abe253.

--- force-app/main/default/lwc/closeCaseOmni/closeCaseOmni.js
+++ force-app/main/default/lwc/closeCaseOmni/closeCaseOmni.js
@@ -118,12 +118,7 @@ export default class CloseCaseOmni extends OmniscriptBaseMixin(
 
   checkFields(detail, reMap) {
     reMap.forEach((field) => {
-      let ele = Object.keys(field)[0];
-      if (ele.includes("Block")) {
-        if (!detail[ele] || (detail[ele] && !detail[ele][ele.split("-")[0]])) {
-          this.missingFields.push(Object.values(field)[0]);
-        }
-      } else if (!detail[ele]) {
+      if (!detail[Object.keys(field)[0]]) {
         this.missingFields.push(Object.values(field)[0]);
       }
     });
