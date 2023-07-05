af720a4fe78005711e0b7bf310698c2fb188c958
vuyyurul <110370451+vuyyurul@users.noreply.github.com>
2023-07-05 17:20:08 +1000

    CC-4295 Close complaint switch (#7090)
    * CC-4295 Close complaint switch
    
    * pmd errors fix

--- force-app/main/default/lwc/closeCase/closeCase.js
+++ force-app/main/default/lwc/closeCase/closeCase.js
@@ -1,4 +1,23 @@
-import { LightningElement } from "lwc";
+import { LightningElement, track } from "lwc";
 import { OmniscriptBaseMixin } from "omnistudio/omniscriptBaseMixin";
 
-export default class closeCase extends OmniscriptBaseMixin(LightningElement) {}
+export default class closeCase extends OmniscriptBaseMixin(LightningElement) {
+  @track showLWC = false;
+  @track showOmni = false;
+
+  connectedCallback() {
+    const params = {
+      input: {},
+      sClassName: "Vlocity_Utils",
+      sMethodName: "getDisableOmni",
+      options: {}
+    };
+    this.omniRemoteCall(params, true).then((res) => {
+      if (res.result.disableOmni) {
+        this.showLWC = true; //if custom setting for disable omni is true, lwc will be displayed
+      } else {
+        this.showOmni = true;
+      }
+    });
+  }
+}
