6c8e466107d5607d3059005e825c893de44d65fe
mohama20 <82633116+mohama20@users.noreply.github.com>
2023-08-09 17:12:41 +1000

    [CC-4904] New field and button for close case omni (#7600)
    * New field for close case omni
    
    * Permission for disable omni field
    
    * Adding the reference to lwc
    
    * New close omni quick action
    
    * Updated complaint record flexi page for new btn
    
    * Panther id update

--- force-app/main/default/lwc/closeCaseOmniBtn/closeCaseOmniBtn.js
+++ force-app/main/default/lwc/closeCaseOmniBtn/closeCaseOmniBtn.js
@@ -1,5 +1,5 @@
 import { LightningElement, api } from "lwc";
 
-export default class closeCase extends LightningElement {
+export default class CloseCaseOmniBtn extends LightningElement {
   @api recordId;
 }
