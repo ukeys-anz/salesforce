05b9fc539295c8f6fdd70a1b4b368bde8e0cff91
sswaraj1204 <131773537+sswaraj1204@users.noreply.github.com>
2023-08-22 12:36:48 +0530

    [ANZX-112847] Display error message when multiple coaches are trying to update Interaction

--- force-app/main/default/lwc/customNextButtonOmni/customNextButtonOmni.js
+++ force-app/main/default/lwc/customNextButtonOmni/customNextButtonOmni.js
@@ -1,9 +1,73 @@
-import { LightningElement } from "lwc";
+import { LightningElement, wire } from "lwc";
+import { getRecord } from "lightning/uiRecordApi";
 import { OmniscriptBaseMixin } from "omnistudio/omniscriptBaseMixin";
+
+const FIELDS = [
+  "Interaction.LastModifiedDate",
+  "Interaction.LastModifiedBy.Name"
+];
 
 export default class CustomNextButtonOmni extends OmniscriptBaseMixin(
   LightningElement
 ) {
+  onLoadLastModifiedDate;
+  latestLastModifiedDate;
+  lastModifiedBy;
+  uiRecordId;
+  boolWireCall = false;
+  showLockRecordError = false;
+  boolShowError = false;
+  errorFromValidation = {};
+
+  /**
+   * Get the recordId from the Omniscript loaded
+   */
+  connectedCallback() {
+    this.uiRecordId = this.omniJsonData.recordId;
+  }
+
+  /**
+   * Get the last modified details of the interaction record
+   */
+  @wire(getRecord, {
+    recordId: "$uiRecordId",
+    fields: FIELDS,
+    refresh: "$boolWireCall"
+  })
+  wireRecord({ data, error }) {
+    if (data) {
+      if (!this.boolWireCall) {
+        if (!this.onLoadLastModifiedDate && !this.lastModifiedBy) {
+          this.onLoadLastModifiedDate = data.fields.LastModifiedDate.value;
+          this.lastModifiedBy =
+            data.fields.LastModifiedBy.value.fields.Name.value;
+        }
+      } else {
+        this.latestLastModifiedDate = data.fields.LastModifiedDate.value;
+        this.lastModifiedBy =
+          data.fields.LastModifiedBy.value.fields.Name.value;
+        if (this.onLoadLastModifiedDate !== this.latestLastModifiedDate) {
+          this.showLockRecordError = true;
+        }
+        // Update the parent JSON by calling omniApplyCallResp
+        if (!this.showLockRecordError) {
+          if (this.boolShowError) {
+            this.boolWireCall = false;
+            this.omniApplyCallResp(this.errorFromValidation);
+          } else {
+            this.omniApplyCallResp(this.errorFromValidation);
+            this.omniNextStep();
+          }
+        }
+      }
+    } else {
+      console.error(
+        "Error in Fetching Interaction LastModified Details -> " +
+          JSON.stringify(error)
+      );
+    }
+  }
+
   saveData(event) {
     if (event) {
       if (this.omniJsonData) {
@@ -11,9 +75,8 @@ export default class CustomNextButtonOmni extends OmniscriptBaseMixin(
         let actualTopics = this.omniJsonData.basenodeForReasonForCV;
         let products = this.omniJsonData.productbasenode;
         let recordTypeName = this.omniJsonData.RecordTypeName;
-        let boolShowError = false;
-        let errorFromValidation = {};
-        errorFromValidation.reasonforvisiterror = false;
+        this.boolShowError = false;
+        this.errorFromValidation.reasonforvisiterror = false;
 
         //Filter only values if the selected is makrked as true
         reasonForVisits = reasonForVisits.filter(
@@ -27,30 +90,32 @@ export default class CustomNextButtonOmni extends OmniscriptBaseMixin(
           (reasonForVisits.length === 0 || reasonForVisits.length > 5) &&
           recordTypeName === "In Person"
         ) {
-          errorFromValidation.reasonforvisiterror = true;
-          boolShowError = true;
+          this.errorFromValidation.reasonforvisiterror = true;
+          this.boolShowError = true;
         }
-        errorFromValidation.topicerror = false;
+        this.errorFromValidation.topicerror = false;
         if (
           (actualTopics.length === 0 || actualTopics.length > 5) &&
           recordTypeName !== "Message"
         ) {
-          errorFromValidation.topicerror = true;
-          boolShowError = true;
+          this.errorFromValidation.topicerror = true;
+          this.boolShowError = true;
         }
-        errorFromValidation.producterror = false;
+        this.errorFromValidation.producterror = false;
         if (products.length === 0 || products.length > 5) {
-          errorFromValidation.producterror = true;
-          boolShowError = true;
+          this.errorFromValidation.producterror = true;
+          this.boolShowError = true;
         }
 
-        // Update the parent JSON by calling omniApplyCallResp
-        if (boolShowError) {
-          this.omniApplyCallResp(errorFromValidation);
-        } else {
-          this.omniApplyCallResp(errorFromValidation);
-          this.omniNextStep();
+        if (!this.uiRecordId) {
+          if (this.boolShowError) {
+            this.omniApplyCallResp(this.errorFromValidation);
+          } else {
+            this.omniApplyCallResp(this.errorFromValidation);
+            this.omniNextStep();
+          }
         }
+        this.boolWireCall = true;
       }
     }
   }
