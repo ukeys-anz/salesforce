869e04195d1a1ea773d0f9d9b3a148d4bd59d5bc
Peter Charalambous <62224246+PeterCharalambousANZ@users.noreply.github.com>
2023-09-18 12:02:43 +1000

    ANZX-91257 Corrected saving ref outcome (#8252)
    * Corrected saving ref outcome
    
    * Undo force ignore comment
    
    * Update cfCreditAssessmentContainer.js
    
    * Updated logic for handling last modified
    
    * Adjusted return statement

--- force-app/main/default/lwc/creditAssessmentModal/creditAssessmentModal.js
+++ force-app/main/default/lwc/creditAssessmentModal/creditAssessmentModal.js
@@ -141,7 +141,7 @@ export default class CreditAssessmentModal extends LightningElement {
           "The Referral Outcome has been updated successfully.",
           "success"
         );
-        pubsub.fire("CreditReferralReasons", "closeFlyout");
+        pubsub.fire("CreditAssessmentParent", "closeFlyout");
       } else {
         this.showToast(
           "Credit Assessment",
