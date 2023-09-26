91e920383dc885d8dbc2bdcd7c226ab2a54473e3
mohama20 <82633116+mohama20@users.noreply.github.com>
2023-09-26 11:12:35 +1000

    CC-5376-Duplicate Check Omni (#8370)
    * Duplicate check omni lwc
    
    * Updated button UI
    
    * Adding duplicate search flow
    
    * Updated complaint record flexi page
    
    * Fixing lint issues in dupe checker lwc
    
    * fixing lint issues
    
    * Added case edit button
    
    * Permission set for dupechecker

---
+++ force-app/main/default/lwc/duplicateCheckOmni/duplicateCheckOmni.js
@@ -0,0 +1,77 @@
+import { OmniscriptBaseMixin } from "omnistudio/omniscriptBaseMixin";
+import { LightningElement, track } from "lwc";
+import { refreshApex } from "@salesforce/apex";
+import tmp from "./duplicateCheckOmni.html";
+
+export default class DuplicateCheckOmni extends OmniscriptBaseMixin(
+  LightningElement
+) {
+  loading = false;
+  showError = false;
+  showResult = false;
+  @track missingFields = [];
+  @track modalMsg;
+  @track showModal = false;
+  @track modalHeader = "Error";
+
+  closeModal() {
+    this.showModal = false;
+  }
+  render() {
+    return tmp;
+  }
+
+  callDuplicateCheck() {
+    this.showModal = false;
+    this.showResult = false;
+    this.missingFields = [];
+    this.modalMsg = "";
+    this.valCustomerFields();
+    if (this.missingFields.length > 0) {
+      this.modalMsg = "Customer Number is required for duplicate search.";
+      this.showModal = true;
+    } else {
+      this.showModal = false;
+      this.showResult = true;
+      const inputsForIP = {
+        customerIdentifier: this.omniJsonData.Case.CustomerDetails
+          .CustomerIdentifier,
+        customerId: this.omniJsonData.Case.CustomerDetails.Customer
+      };
+
+      const params = {
+        input: JSON.stringify(inputsForIP),
+        sClassName: "omnistudio.IntegrationProcedureService",
+        sMethodName: "Case_getInfo",
+        options: {}
+      };
+
+      this.omniRemoteCall(params, true)
+        .then((response) => {
+          let res = JSON.parse(JSON.stringify(response));
+          this.omniApplyCallResp(res.result.IPResult);
+        })
+        .catch((error) => {
+          window.console.log(error, "error");
+        });
+      return refreshApex(this.record);
+    }
+    return null;
+  }
+  // Set input for duplicate search flow.
+  get inputVariables() {
+    return [
+      {
+        name: "CustomerNumber",
+        type: "String",
+        value: this.omniJsonData.Case.CustomerDetails.Customer
+      }
+    ];
+  }
+  // validate that the required field Customer Number for dupe search has been provided
+  valCustomerFields() {
+    if (!this.omniJsonData.Case.CustomerDetails.Customer) {
+      this.missingFields.push("Customer Number");
+    }
+  }
+}
