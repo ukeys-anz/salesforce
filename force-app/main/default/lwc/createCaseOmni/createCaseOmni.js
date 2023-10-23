b2ed2cdae6d44925b91977b2ee419f8552bd6f35
vuyyurul <110370451+vuyyurul@users.noreply.github.com>
2023-10-23 15:47:35 +1100

    CC-2347 Complaint resolution epic (#8858)
    * CC-2347 Complaint Resolution epic
    
    * build errors fix
    
    * errors fix
    
    * sysl errors fix
    
    * permission set changes
    
    * permission set changes
    
    * errors fix
    
    * permission level changes
    
    * CC-2347 Refine createCMOSUser() to recalculate PSG
    
    * CC-2347 Must recalculate PSG before using it.
    
    * field label change
    
    * permission level changes
    
    * errors fix
    
    * CC-2347 Complaint Resolution epic
    
    * adding new field
    
    * adding field to panther
    
    * panther file changes
    
    * history tracking
    
    * CC-2347 Complaint Resolution epic(omni)
    
    * reverting layout changes
    
    * validation rule changes
    
    ---------
    
    Co-authored-by: Gregory Shaw <Greg.Shaw@anz.com>

--- force-app/main/default/lwc/createCaseOmni/createCaseOmni.js
+++ force-app/main/default/lwc/createCaseOmni/createCaseOmni.js
@@ -198,6 +198,13 @@ export default class CreateCaseOmni extends OmniscriptBaseMixin(
       !details.rewardPoints1
     )
       this.missingFields.push("Reward Points 1");
+    if (
+      (FINANCIAL_REMEDY.includes(details.ComplaintRemedy1) ||
+        SUB_REMS_PAYMENT.includes(details.ComplaintSubRemedy1)) &&
+      !details.PaymentAmountProvided1
+    ) {
+      this.missingFields.push("Payment Amount Provided 1");
+    }
     if (details.secondComplaintRemedyCheckbox === "Yes") {
       this.checkFields(details, this.omniJsonData.secRemedyMap);
       if (
@@ -219,6 +226,13 @@ export default class CreateCaseOmni extends OmniscriptBaseMixin(
         !details.rewardPoints2
       )
         this.missingFields.push("Reward Points 2");
+      if (
+        (FINANCIAL_REMEDY.includes(details.ComplaintRemedy2) ||
+          SUB_REMS_PAYMENT.includes(details.ComplaintSubRemedy2)) &&
+        !details.PaymentAmountProvided2
+      ) {
+        this.missingFields.push("Payment Amount Provided 2");
+      }
     }
     if (details.thirdComplaintRemedyCheckbox === "Yes") {
       this.checkFields(details, this.omniJsonData.thirdRemedyMap);
@@ -241,6 +255,13 @@ export default class CreateCaseOmni extends OmniscriptBaseMixin(
         !details.rewardPoints3
       )
         this.missingFields.push("Reward Points 3");
+      if (
+        (FINANCIAL_REMEDY.includes(details.ComplaintRemedy3) ||
+          SUB_REMS_PAYMENT.includes(details.ComplaintSubRemedy3)) &&
+        !details.PaymentAmountProvided3
+      ) {
+        this.missingFields.push("Payment Amount Provided 3");
+      }
     }
     if (details.ComplaintStatus === "Escalated")
       this.checkFields(details, this.omniJsonData.escMap);
