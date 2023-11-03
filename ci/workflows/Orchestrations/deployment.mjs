ff6d543a6d8b80950018840ee279ba721d665355
FaridMasjediANZ <88009478+FaridMasjediANZ@users.noreply.github.com>
2023-11-03 14:47:59 +1100

    [ANZX-118964] CI, CD, Run All Tests Workflow and  Release Pipeline | Update services, orchestration, and helper (#9066)
    * Update Orchestration | Validation | Check Draft Pr on code coverage
    
    * Update Service | artifact | clean logger
    
    * Update Service | deploy | Update: check prev job status, update codeCoverage
    
    * Update Service | helper | Update: renameFile, just destructive changes
    
    * Update helper | read destructive, show ignored files, update logger
    
    * Update Service | artifact | print all changed and ignored files, update artifact sgd on validate
    
    * Update Service | deploy | Update validate, deploy progress: to not exit after default 33 mins
    
    * Update Orchestration | Validation
    
    * Update Orchestration | deployment
    
    * Update Orchestration | quickDeployment
    
    * Update Orchestration | release
    
    * Update Workflow | Validation
    
    * Update Workflow | Deployment
    
    * Update Workflow | Quick Deployment
    
    * Update Workflow | Release
    
    * Update Service | artifact | create right artifact
    
    * Update on Run all tests | not exit after 33 mins
    
    * Update logger
    
    * Update quick deployment orchestration
    
    * Clean & Add comments
    
    * Add deployReport if there is any error
    
    * rename a method

--- ci/workflows/Orchestrations/deployment.mjs
+++ ci/workflows/Orchestrations/deployment.mjs
@@ -22,12 +22,16 @@ const {
   BASE_REF,
   BASE_REF_LAST_TAG,
   SFDX_URL,
-  RUN_ID
+  RUN_ID,
+  WORKING_DIR
 } = process.env;
 
 const PROPER_FOLDER_NAME = renameItem(BASE_REF);
 const SOURCE_DIR = `artifact-${PROPER_FOLDER_NAME}-${RUN_ID}`;
-
+const ARTIFACT_PACKAGE_XML =
+  WORKING_DIR + "/" + SOURCE_DIR + "/package/package.xml";
+const ARTIFACT_DESTRUCTIVE_XML =
+  WORKING_DIR + "/" + SOURCE_DIR + "/destructiveChanges/destructiveChanges.xml";
 //////////
 
 /// functions
@@ -36,7 +40,12 @@ const deployment = () => {
   createDiffOnDeploy(SOURCE_DIR, BASE_REF, BASE_REF_LAST_TAG);
   authenticate(BASE_REF, SFDX_URL);
   const deployment = deployWithoutTest(BASE_REF, SOURCE_DIR);
-  deployProgress(deployment);
+  deployProgress(
+    deployment,
+    BASE_REF,
+    ARTIFACT_PACKAGE_XML,
+    ARTIFACT_DESTRUCTIVE_XML
+  );
 };
 
 const clean = () => {
