19072c186d5d4e858960c7c9246cd300ec0abd1a
ankitanair5 <ankita.nair2@anz.com>
2024-02-05 20:23:10 +0530

    Revert "Changes for Checking against Original values (#9958)"
    This reverts commit e0a858aac0d6eca321963e7b8022ae9101cfaf34.

--- force-app/main/default/lwc/closeCaseOmni/closeCaseOmni.js
+++ force-app/main/default/lwc/closeCaseOmni/closeCaseOmni.js
@@ -73,18 +73,16 @@ export default class CloseCaseOmni extends OmniscriptBaseMixin(
   validateIssueTypeFieldValues() {
     let details = this.omniJsonData.Case;
     let issueTypeCombinationMap = this.omniJsonData.issueTypeCombinationMap;
-    let originalValues = this.omniJsonData.originalValues;
     let proceed = false;
-    if (
-      originalValues.Type !== details.Type ||
-      originalValues.IDR_Subsequent_Issue__c !== details.IDR_Subsequent_Issue__c
-    ) {
-      if (details.Type in issueTypeCombinationMap) {
-        let issueTypeArray = issueTypeCombinationMap[details.Type];
-        if (issueTypeArray.includes(details.IDR_Subsequent_Issue__c)) {
-          proceed = true;
-        }
+    if (details.Type in issueTypeCombinationMap) {
+      let issueTypeArray = issueTypeCombinationMap[details.Type];
+      if (issueTypeArray.includes(details.IDR_Subsequent_Issue__c)) {
+        proceed = true;
+      } else {
+        proceed = false;
       }
+    } else {
+      proceed = false;
     }
     if (proceed) {
       this.showModal = true;
