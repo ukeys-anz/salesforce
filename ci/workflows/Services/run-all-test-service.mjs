ef945bf4d70c34bb4288487d4c5dc9f8bbbb34e3
singhn60 <Nagendra.Singh@anz.com>
2024-02-12 15:33:54 +0530

    Revert "Check error on runAllTestsProcess"
    This reverts commit f71060c8d8908a1d864852d9e938f11f8e4169e5.

--- ci/workflows/Services/run-all-test-service.mjs
+++ ci/workflows/Services/run-all-test-service.mjs
@@ -111,11 +111,6 @@ const runAllLocalTestsProgress = (
       process.exit(1);
     }
   });
-
-  runAllTestsProcess.on("exit", (code, signal) => {
-    if (code) console.log(`Process exit with code : ${code}`);
-    if (signal) console.log(`Process killed with signal : ${signal}`);
-  });
 };
 
 /////
