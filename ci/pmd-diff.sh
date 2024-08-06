a31fd84949192d77f614d057064ea78b9f4e818f
FaridMasjediANZ <88009478+FaridMasjediANZ@users.noreply.github.com>
2024-08-06 19:16:14 +1000

    [AR-153378] Fix pmd issues (#12574)
    * [AR-153378] Update bash file | store all filepath even with space on filepath
    
    * [AR-153378] Update warning.xml | remove an extra rule ( pmd v.7.4.0 )
    
    * Update code_scan pipeline to check the if there any changes using change number

--- ci/pmd-diff.sh
+++ ci/pmd-diff.sh
@@ -44,11 +44,28 @@ if [ "${CHANGED_FILES}" -gt "0" ]; then
 fi
 
 allFilesPath=""
-if [[ -d ./tmpPMD/deploy ]];then
-    allFiles=$( find ./tmpPMD -type f -print )
-    for f in ${allFiles[*]}; do
-        allFilesPath="$allFilesPath""${f/\/tmpPMD\/deploy} "
+tempFile=$(mktemp)  # Create a temporary file
+
+# Ensure the temporary file is cleaned up on exit
+trap 'rm -f "$tempFile"' EXIT
+
+if [[ -d ./tmpPMD/deploy ]]; then
+    # Use find with -print0 to handle spaces and special characters
+    find ./tmpPMD -type f -print0 | while IFS= read -r -d '' f; do
+        # Correct prefix removal
+        strippedPath="${f/\/tmpPMD\/deploy/}"
+        quotedPath=$(printf '"%s"' "$strippedPath")
+        # Append to the temporary file
+        echo "$quotedPath" >> "$tempFile"
     done
+
+    # Read all paths from the temporary file and concatenate them
+    while IFS= read -r line; do
+        allFilesPath+="$line "
+    done < "$tempFile"
 fi
 
+# Remove trailing space
+allFilesPath=$(echo "$allFilesPath" | sed 's/ $//')
+echo CHANGED_FILES="$CHANGED_FILES" >> $GITHUB_ENV
 echo allFilesPath="$allFilesPath" >> $GITHUB_ENV
