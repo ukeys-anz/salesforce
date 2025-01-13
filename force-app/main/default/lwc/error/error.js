2118e96b4a386e25563502ed38f3a89b094d21a9
swarajs2 <shivam.swaraj@anz.com>
2025-01-13 09:12:56 +0530

    Added Error.js component

--- force-app/main/default/lwc/error/error.js
+++ force-app/main/default/lwc/error/error.js
@@ -5,9 +5,24 @@ export default class Error extends LightningElement {
   @api code;
   @api permissionIssue;
   @api errorImageUrl;
+  @api customClassFromParent;
+  errorClass = "slds-card slds-grid slds-grid_vertical-align-center";
+  classesForText =
+    "slds-text-align_center slds-text-color_weak slds-text-heading_small";
+  emojiClass = "emoji";
+  emojiSize = "large";
+  iconVariant = "default";
   errorMessage;
 
   connectedCallback() {
+    if (this.customClassFromParent) {
+      this.errorClass = `${this.errorClass} ${this.customClassFromParent}`;
+      this.classesForText =
+        "slds-text-align_center slds-text-color_white slds-text-body_regular";
+      this.emojiClass = "emojiSmall";
+      this.emojiSize = "small";
+      this.iconVariant = "inverse";
+    }
     if (this.permissionIssue) {
       this.errorMessage = `Don’t worry, it's not broken. You need an extra permission level to view this.`;
     }
