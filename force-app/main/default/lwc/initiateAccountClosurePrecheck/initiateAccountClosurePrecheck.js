a7034b5f3885c0a5fb3e351935589cb45ed189a6
swarajs2 <shivam.swaraj@anz.com>
2025-01-28 11:30:10 +0530

    [ANZX-199411] Adding review comments and optimizing code

--- force-app/main/default/lwc/initiateAccountClosurePrecheck/initiateAccountClosurePrecheck.js
+++ force-app/main/default/lwc/initiateAccountClosurePrecheck/initiateAccountClosurePrecheck.js
@@ -1,6 +1,6 @@
 import { LightningElement, api, wire } from "lwc";
 import fetchEligibleCasesForPrecheck from "@salesforce/apex/AccountClosureController.fetchEligibleCasesForPrecheck";
-import initiateAccountClosurePrecheck from "@salesforce/apex/AccountClosureController.initiateAccountClosurePrecheck";
+import initiateAccountClosurePrecheck from "@salesforce/apex/AccountClosureController.initiateAccountClosurePrechecks";
 import processResponsesAndUpdateCases from "@salesforce/apex/AccountClosureController.processResponsesAndUpdateCases";
 import { CloseActionScreenEvent } from "lightning/actions";
 
@@ -135,9 +135,10 @@ export default class InitiateAccountClosurePrecheck extends LightningElement {
 
   async processPrecheckResponse(records) {
     try {
+      const processedResponses = this.generateResponseToProcess(records);
       const childCasesDetailsPostPrecheck =
         await processResponsesAndUpdateCases({
-          fabricSealResponseList: records,
+          accountClosurePrecheckResponse: processedResponses,
           eligibleCasesForPrecheck: this.eligibleChildCases
         });
 
@@ -217,5 +218,33 @@ export default class InitiateAccountClosurePrecheck extends LightningElement {
       isSuccessIcon: isSuccessIcon,
       workFlow: caseRecord.Status
     }));
+  }
+
+  generateResponseToProcess(records) {
+    const processedResponses = records.map((response) => {
+      const {
+        salesforceCaseNumber,
+        acceptance,
+        unsatisfiedPreconditions,
+        businessProcessId
+      } = response;
+      let failedReasons = [];
+
+      if (acceptance === "ACCEPTANCE_REJECTED" && unsatisfiedPreconditions) {
+        Object.keys(unsatisfiedPreconditions).forEach((precheckKey) => {
+          const precondition = unsatisfiedPreconditions[precheckKey];
+          if (precondition?.eligibility === "ELIGIBILITY_INELIGIBLE") {
+            failedReasons.push(precheckKey);
+          }
+        });
+      }
+      return {
+        salesforceCaseNumber,
+        acceptance,
+        failedReasons,
+        businessProcessId
+      };
+    });
+    return processedResponses;
   }
 }
