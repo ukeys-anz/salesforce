55d1b1f1296920c9e67a6dce4637c972f8957a22
prahladbhadani29 <174101214+prahladbhadani29@users.noreply.github.com>
2025-02-11 03:34:57 +0530

    CC-12570 CC-12528 ML defects and Feeback | MLCRM -> Release | 19th Feb (#15196)
    Feature/CC-12235 - Enable creation of new list views
    Feature/CC-11001 - Global search on customer relationship object (#14171)
    * CC-12570 and CC-12479
    * CC-12782 Updated Transaction History Lookup Label for PCA

--- force-app/main/default/lwc/opportunityCreateOpenLoanapp/opportunityCreateOpenLoanapp.js
+++ force-app/main/default/lwc/opportunityCreateOpenLoanapp/opportunityCreateOpenLoanapp.js
@@ -21,8 +21,6 @@ import LoanappPrerequisiteLabel from "@salesforce/label/c.LoanappPrerequisiteLab
 import LoanappFriendlyErrorMessage from "@salesforce/label/c.LoanappFriendlyErrorMessage";
 
 const USER_FRIENDLY_ERROR = LoanappFriendlyErrorMessage;
-const DUPLICATE_LOANAPP_REF_ERROR =
-  "Duplicate Loanapp Reference number found! Please check and update existing records and try again.";
 const VALID_CUSTOMER_NEEDS = [
   "Home Loan - Bridging Finance",
   "Home Loan - Refinance",
@@ -165,9 +163,11 @@ export default class OpportunityCreateOpenLoanapp extends LightningElement {
         return;
       }
       this.userFriendlyErrorMessage = error.body.message.includes(
-        "DUPLICATE_VALUE"
+        "FIELD_CUSTOM_VALIDATION_EXCEPTION"
       )
-        ? DUPLICATE_LOANAPP_REF_ERROR
+        ? error.body.message.substring(
+            82 + "FIELD_CUSTOM_VALIDATION_EXCEPTION".length
+          )
         : `${USER_FRIENDLY_ERROR}: ${error.body.message}`;
       this.showError = true;
     }
