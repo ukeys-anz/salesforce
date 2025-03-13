48dd418a2c5164fcb6d5454fe1ba421bb5b3f4da
prahladbhadani29 <174101214+prahladbhadani29@users.noreply.github.com>
2025-03-13 22:55:46 +0530

    CC-13045 Bug Fix CC-13363 and CC-13056 to develop (#15919)
    * CC-13045 relationship tab removed from Organization customer page (#15456)
    * CC-13363 Reports and Dashboard should be visible under Menu Navigation of Mobile Lending App
    * CC-13056 Territory new attributes field access.

--- force-app/main/default/lwc/financialSummaryViewAsync/financialSummaryViewAsync.js
+++ force-app/main/default/lwc/financialSummaryViewAsync/financialSummaryViewAsync.js
@@ -18,8 +18,6 @@ const TOTAL_CUSTOMER_LIMIT = "TOTAL_CUSTOMER_LIMIT";
 const CLG_CREDIT_RISK = "CLG_CREDIT_RISK";
 const TOTAL_MORTAGE_LIMIT = "TOTAL_MORTAGE_LIMIT";
 const CCRM_PROFILE = "ANZ CCRM Standard User";
-const ML_USER_PROFILE = "ANZ ML Standard User";
-const ADMIN_PROFILE = "System Administrator";
 
 export default class financialSummaryViewAsync extends LightningElement {
   @api recordId;
@@ -202,13 +200,6 @@ export default class financialSummaryViewAsync extends LightningElement {
     } else {
       this.handleNoAvailableBalance(this.financialSummaryData);
     }
-  }
-
-  get showCCCalculationMsg() {
-    return (
-      this.currentUserProfile === ML_USER_PROFILE ||
-      this.currentUserProfile === ADMIN_PROFILE
-    );
   }
 
   handleSubscribe() {
