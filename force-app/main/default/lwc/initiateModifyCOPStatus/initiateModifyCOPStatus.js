4303ecf604298adae45d7a285e113a7b5dc8116d
kalaln <132044196+kalaln@users.noreply.github.com>
2025-03-19 23:10:21 +0530

    [ANZX-208176] COP UI with Backend Controller (#16057)
    * [ANZX-208176] COP UI with Backend Controller
    
    * Validation issues resolution
    
    * issue resolution
    
    * JEST resolution
    
    * Validation Issue Resolution
    
    * COP helper Resolution
    
    * LWC issue fix on epic
    
    * Not needed lists removed from the code
    
    * Optimisation
    
    * PMD Error
    
    * Prettier
    
    * Comment added on LWC for prettier issue
    
    ---------
    
    Co-authored-by: guptaa94 <132050402+guptaa94@users.noreply.github.com>

---
+++ force-app/main/default/lwc/initiateModifyCOPStatus/initiateModifyCOPStatus.js
@@ -0,0 +1,192 @@
+import { LightningElement, api, wire } from "lwc";
+import { getRecord } from "lightning/uiRecordApi";
+import fetchChildCasesCoP from "@salesforce/apex/ConfirmationOfPayeeController.fetchChildCasesCoP";
+import initiateModifyConfirmationOfPayee from "@salesforce/apex/ConfirmationOfPayeeController.initiateModifyConfirmationOfPayee";
+import { CloseActionScreenEvent } from "lightning/actions";
+import { getFocusedTabInfo, refreshTab } from "lightning/platformWorkspaceApi";
+
+const fields = ["Case.Account.OCV_ID__c"];
+
+const caseColumns = [
+  { label: "Product", fieldName: "product" },
+  { label: "Account Number", fieldName: "accountNumber" },
+  { label: "Account Type", fieldName: "accountType" },
+  { label: "Current COP Status", fieldName: "currCOPStatus" },
+  { label: "Requested COP Status", fieldName: "reqCOPStatus" },
+  {
+    label: "Child Case Number",
+    fieldName: "childCaseNumberUrl",
+    type: "url",
+    typeAttributes: { label: { fieldName: "childCaseNumber" } }
+  }
+];
+
+const responseColumns = [
+  { label: "Product", fieldName: "productName" },
+  { label: "Account Number", fieldName: "accountNumber" },
+  { label: "Account Type", fieldName: "accountType" },
+  {
+    label: "Child Case Number",
+    fieldName: "childCaseNumberUrl",
+    type: "url",
+    typeAttributes: { label: { fieldName: "caseNumber" } }
+  }
+];
+export default class InitiateModifyCOPStatus extends LightningElement {
+  caseColumns = caseColumns;
+  responseColumns = responseColumns;
+  customerOcvId;
+  casesData = [];
+  eligibleChildCases = [];
+  successfulCases = [];
+  failureCases = [];
+  loading = false;
+  hasFetchedCases = false;
+  initialLoading = true;
+  isModificationSuccess = false;
+  isModificationFailed = false;
+  hasError = false;
+  errorMsg;
+  @api recordId;
+  successData = [];
+  failureData = [];
+  showNoDataMessage = false;
+  showPreviewMessage = false;
+
+  issueTypeCOPMap = {
+    "Confirmation of Payee Opt-Out": {
+      currentValue: "OPT-IN",
+      requestedValue: "OPT-OUT"
+    },
+    "Confirmation of Payee Opt-In": {
+      currentValue: "OPT-OUT",
+      requestedValue: "OPT-IN"
+    }
+  };
+
+  get showPreviewSection() {
+    return (
+      !this.isModificationSuccess &&
+      !this.isModificationFailed &&
+      this.showPreviewMessage
+    );
+  }
+
+  get showSuccessSection() {
+    return this.isModificationSuccess;
+  }
+
+  get showFailureSection() {
+    return this.isModificationFailed;
+  }
+
+  get isDataLoading() {
+    return !this.initialLoading && !this.isModificationSuccess;
+  }
+
+  get isModificationNotDone() {
+    return (
+      !this.isModificationSuccess &&
+      !this.isModificationFailed &&
+      !this.showNoDataMessage
+    );
+  }
+
+  get showLoading() {
+    return this.initialLoading || this.loading;
+  }
+
+  @wire(getRecord, { recordId: "$recordId", fields })
+  wiredData({ data }) {
+    try {
+      if (data && !this.hasFetchedCases) {
+        this.customerOcvId =
+          data.fields?.Account?.value?.fields?.OCV_ID__c?.value;
+        if (this.customerOcvId) {
+          this.hasFetchedCases = true;
+          this.getEligibleChildCasesForCOPStatusUpdate();
+        }
+      }
+    } catch (error) {
+      this.handleError(error);
+    }
+  }
+
+  async getEligibleChildCasesForCOPStatusUpdate() {
+    try {
+      const caseDetails = await fetchChildCasesCoP({
+        parentCaseId: this.recordId
+      });
+      if (caseDetails && caseDetails.length > 0) {
+        this.eligibleChildCases = caseDetails;
+        this.casesData = this.generateData(caseDetails);
+        this.showPreviewMessage = true;
+      } else {
+        this.showNoDataMessage = true;
+      }
+      this.initialLoading = false;
+    } catch (error) {
+      this.handleError();
+    }
+  }
+
+  async handleModifyCOPStatus() {
+    this.loading = true;
+    this.hasError = false;
+    try {
+      const response = await initiateModifyConfirmationOfPayee({
+        customerOcvId: this.customerOcvId,
+        eligibleCases: this.eligibleChildCases
+      });
+      if (response?.successList?.length > 0) {
+        this.successData = response.successList;
+        this.isModificationSuccess = true;
+      }
+      if (response?.failureList?.length > 0) {
+        this.failureData = response.failureList;
+        this.isModificationFailed = true;
+      }
+      await this.refreshTab();
+    } catch (error) {
+      this.handleError();
+    } finally {
+      this.loading = false;
+    }
+  }
+
+  handleCancel() {
+    this.dispatchEvent(new CloseActionScreenEvent());
+  }
+
+  handleError() {
+    this.hasError = true;
+    this.errorMsg =
+      "Please try again. Raise a fault through TechAssist if the problem persists.";
+  }
+
+  generateData(caseRecords) {
+    return caseRecords.map((caseRecord) => ({
+      id: caseRecord.Id,
+      product:
+        caseRecord?.FinServ__FinancialAccount__r?.Product_Name__c || null,
+      accountNumber:
+        caseRecord?.FinServ__FinancialAccount__r
+          ?.FinServ__FinancialAccountNumber__c || null,
+      accountType:
+        caseRecord?.FinServ__FinancialAccount__r.Ownership__c === "Individual"
+          ? "Sole"
+          : caseRecord?.FinServ__FinancialAccount__r.Ownership__c,
+      childCaseNumber: "#" + caseRecord.CaseNumber,
+      childCaseNumberUrl: "/" + caseRecord.Id,
+      currCOPStatus: this.issueTypeCOPMap[caseRecord.Parent.Type].currentValue,
+      reqCOPStatus: this.issueTypeCOPMap[caseRecord.Parent.Type].requestedValue
+    }));
+  }
+
+  async refreshTab() {
+    const { tabId } = await getFocusedTabInfo();
+    await refreshTab(tabId, {
+      includeAllSubtabs: false
+    });
+  }
+}
