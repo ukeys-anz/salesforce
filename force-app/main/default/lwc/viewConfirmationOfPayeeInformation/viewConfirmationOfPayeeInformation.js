5764811e8cfc553fc23c79e724068f9e9d5a80f0
sswaraj1204 <shivam.swaraj@anz.com>
2025-03-25 17:39:16 +0530

    [ANZX-214393] Resolving review comments for GetHelpCaseActions and adding missing financial account record page changes (#16188)
    * [ANZX-214393] Resolving review comments for GetHelpCaseActions and adding missing financial account record page changes
    
    * New LWC on Flexipage and Update in Chatter post
    
    * Lint Issue fix
    
    * Tooltip update
    
    * Prettier issue fix
    
    ---------
    
    Co-authored-by: kalaln <Nilesh.Kalal@anz.com>

---
+++ force-app/main/default/lwc/viewConfirmationOfPayeeInformation/viewConfirmationOfPayeeInformation.js
@@ -0,0 +1,35 @@
+import { LightningElement, api } from "lwc";
+import fetchCOPInformation from "@salesforce/apex/ConfirmationOfPayeeController.fetchCOPInformation";
+
+export default class ViewConfirmationOfPayeeInformation extends LightningElement {
+  @api recordId;
+  status;
+  requestedBy;
+  modifiedBy;
+  dateTimeValue;
+  showData = false;
+  isLoading = false;
+  error;
+
+  handleButtonClick() {
+    this.isLoading = true;
+    fetchCOPInformation({ financialAccountId: this.recordId })
+      .then((dataList) => {
+        if (dataList && dataList.length > 0) {
+          const data = dataList[0];
+          this.showData = true;
+          this.status = data.accountStatusCOP;
+          this.requestedBy = data.requestedByName;
+          this.modifiedBy = data.modifiedByName;
+          this.dateTimeValue = data.statusChangeDateTime;
+          this.isLoading = false;
+        }
+      })
+      .catch((error) => {
+        this.error = error;
+        this.showData = false;
+        this.isLoading = false;
+        console.error("Error fetching COP information:", error);
+      });
+  }
+}
