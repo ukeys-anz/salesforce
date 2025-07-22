d17fa330a2f55eac614e8da48dca946244e5433d
kalaln <Nilesh.Kalal@anz.com>
2025-07-22 16:02:37 +0530

    Origin needs to be removed

--- force-app/main/default/lwc/financialAccount/financialAccount.js
+++ force-app/main/default/lwc/financialAccount/financialAccount.js
@@ -72,7 +72,7 @@ export default class FinancialAccount extends NavigationMixin(
           finAccount.showMultipartyBadge = true;
           finAccount.finserv_ownership = "Joint";
         }
-        if (finAccount.product_details.origin === "PRODUCT_ORIGIN_ARC") {
+        if (finAccount.product_details?.marketing_code) {
           finAccount.isClassicAccount = true;
         }
         return finAccount;
