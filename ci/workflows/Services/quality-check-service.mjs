7d6baf43942ea0a1e8b4abd9c21806539da841f2
FaridMasjediANZ <88009478+FaridMasjediANZ@users.noreply.github.com>
2025-07-23 11:40:14 +1000

    [AR-224988] Update detect changes code (#18643)

--- ci/workflows/Services/quality-check-service.mjs
+++ ci/workflows/Services/quality-check-service.mjs
@@ -28,14 +28,13 @@ const findDiffOnPR = (baseRef, headRef) => {
   }
 
   loggerInStep(`
-        Base ref: ${baseRef}
-        Head ref: ${headRef}
-        Merge base: ${mergeBase}
-    `);
+    Base ref: ${baseRef}
+    Head ref: ${headRef}
+    Merge base: ${mergeBase}
+  `);
 
-  const changedFilesRaw = runDiffCommand(
-    `git diff --name-only ${mergeBase} ${headRef}`
-  );
+  const diffCommand = `git diff --name-only ${mergeBase} ${headRef}`;
+  const changedFilesRaw = runDiffCommand(diffCommand);
 
   if (!changedFilesRaw) {
     loggerInStep("No changed files detected");
