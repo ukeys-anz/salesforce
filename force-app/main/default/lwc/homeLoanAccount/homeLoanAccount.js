7250570bff56a91e2639ccc091d77535120dd9e0
kalaln <Nilesh.Kalal@anz.com>
2025-07-31 17:08:29 +0530

    prettier issue resolution

--- force-app/main/default/lwc/homeLoanAccount/homeLoanAccount.js
+++ force-app/main/default/lwc/homeLoanAccount/homeLoanAccount.js
@@ -85,7 +85,8 @@ export default class HomeLoanAccountCard extends NavigationMixin(
                 finAccount.recordId = account.FinServ__FinancialAccount__c;
               }
             });
-            finAccount.showOffsetDetails = finAccount.product_details?.marketing_code;
+            finAccount.showOffsetDetails =
+              finAccount.product_details?.marketing_code;
             finAccount.offsetDetails = this.handleOffsetDetails(
               finAccount,
               this.offsetDetails
@@ -109,7 +110,8 @@ export default class HomeLoanAccountCard extends NavigationMixin(
 
         if (this.isFinAccountTab) {
           this.singleFinAccount = this.financialAccounts[0];
-          this.singleFinAccount.showOffsetDetails = this.singleFinAccount.product_details?.marketing_code;
+          this.singleFinAccount.showOffsetDetails =
+            this.singleFinAccount.product_details?.marketing_code;
           this.offsetList = this.handleOffsetDetails(
             this.singleFinAccount,
             this.offsetDetails
