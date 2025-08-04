f24155f5d0e8665209e3c82622455bbaa9c11353
kalaln <Nilesh.Kalal@anz.com>
2025-08-04 09:42:54 +0530

    Icon changes

--- force-app/main/default/lwc/financialAccountHeader/financialAccountHeader.js
+++ force-app/main/default/lwc/financialAccountHeader/financialAccountHeader.js
@@ -20,6 +20,7 @@ export default class FinancialAccount extends NavigationMixin(
   @api titleIcon;
   @api iconColor;
   @api productCode;
+  @api accountType;
   showInfoModal = false;
   productTitle;
 
@@ -57,7 +58,11 @@ export default class FinancialAccount extends NavigationMixin(
   get processedFinAccounts() {
     return this.handleAccountInformation(this.accountDetails);
   }
-
+  get iconWrapperClass() {
+    return this.accountType === "Card"
+      ? `${this.iconColor} card-icon-color`
+      : this.iconColor;
+  }
   handleAccountInformation(finAccounts) {
     // As per story ANZX-113310 Colour of status “Active”, “Dormant“, “Closed” is changed .Hence, changing the badge class
     if (!finAccounts) {
