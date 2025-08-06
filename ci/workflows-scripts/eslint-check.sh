19811e5262a860198cd7a23cccb1d9d9a690384d
FaridMasjediANZ <88009478+FaridMasjediANZ@users.noreply.github.com>
2025-08-06 16:43:12 +1000

    Merge pull request #18905 from anzx/update-eslint-check-msg
    [AR-228541] Update eslint check script

--- ci/workflows-scripts/eslint-check.sh
+++ ci/workflows-scripts/eslint-check.sh
@@ -60,7 +60,7 @@ if [[ "$eslint_failed" == "true" ]]; then
   {
     echo "result<<EOF"
     echo "<details><summary>❌ ESLint check failed</summary><pre>"
-    cat "$eslint_output_file"
+    sed 's/potentially fixable with the `--fix` option//g' "$eslint_output_file"
     echo "</pre></details>"
     echo "EOF"
   } >> "$GITHUB_OUTPUT"
