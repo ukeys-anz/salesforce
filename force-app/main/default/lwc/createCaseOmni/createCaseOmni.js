8f059191fbe0f61fd4e83db40df4aff84f326c2a
PiyushKumar-Anz <154223424+PiyushKumar-Anz@users.noreply.github.com>
2025-09-26 20:36:18 +0530

    Fix/CC-14804- Complaint-Address-Lookup (#19776)
    * edit screen changes
    
    * Address Case Creation Screen
    
    * case creation omni
    
    * omni components
    
    * prettier fix
    
    * prettier and lint
    
    ---------
    
    Co-authored-by: anuja7792 <154575587+anuja7792@users.noreply.github.com>
    Co-authored-by: Anuja Vats <anuja.vats@anz.com>
    Co-authored-by: ManishaSrilekha03 <manishasrilekha.pokala@anz.com>

--- force-app/main/default/lwc/createCaseOmni/createCaseOmni.js
+++ force-app/main/default/lwc/createCaseOmni/createCaseOmni.js
@@ -65,6 +65,11 @@ export default class CreateCaseOmni extends OmniscriptBaseMixin(
     } else if (this.checkOCVDown()) {
       this.modalMsg +=
         "Customer details cannot be retrieved as One Customer View (OCV) is currently unavailable.";
+    } else if (this.checkAddressSearch()) {
+      this.modalMsg +=
+        "Please Fill the Address Details by Selecting Address from Search";
+    } else if (this.checkValidPostCode()) {
+      this.modalMsg += "Please Enter Valid PostCode";
     } else if (this.validateRealFormID()) {
       handleErrorShowToast(
         this,
@@ -223,6 +228,77 @@ export default class CreateCaseOmni extends OmniscriptBaseMixin(
       this.omniJsonData.IsOCVDown === true &&
       this.omniJsonData.Case.isThisCustomerComplaint === "Yes" &&
       !this.omniJsonData.isEligibleAppForLookUp
+    ) {
+      return true;
+    }
+    return false;
+  }
+  checkAddressSearch() {
+    if (
+      this.omniJsonData.Case.isThisCustomerComplaint === "No" &&
+      this.omniJsonData.Case.CustomerDetails?.SearchAddressRadio ===
+        "Search Address" &&
+      !Object.prototype.hasOwnProperty.call(
+        this.omniJsonData.Case,
+        "disablAddress"
+      )
+    ) {
+      return true;
+    }
+    if (
+      this.omniJsonData.Case.CustomerDetails.thirdPartyRepCheckbox === "Yes" &&
+      this.omniJsonData.Case.CustomerDetails?.thirdPartyAddress ===
+        "Search Address" &&
+      !Object.prototype.hasOwnProperty.call(
+        this.omniJsonData.Case,
+        "disablThirdAddress"
+      )
+    ) {
+      return true;
+    }
+    return false;
+  }
+  checkValidPostCode() {
+    let compare = /^[0-9]{4}$/;
+    let postCode =
+      "" + this.omniJsonData.Case.CustomerDetails?.PostcodeReadOnly;
+    let thirdPartyCode =
+      "" + this.omniJsonData.Case.CustomerDetails?.thirdPartyPostCode;
+    if (
+      this.omniJsonData.Case.isThisCustomerComplaint === "No" &&
+      this.omniJsonData.Case.CustomerDetails?.SearchAddressRadio ===
+        "Manual Address Entry" &&
+      (!Object.prototype.hasOwnProperty.call(
+        this.omniJsonData.Case.CustomerDetails,
+        "PostcodeReadOnly"
+      ) ||
+        postCode === "" ||
+        !this.omniJsonData.Case?.validAddress)
+    ) {
+      return true;
+    }
+    if (
+      this.omniJsonData.Case.CustomerDetails.thirdPartyRepCheckbox === "Yes" &&
+      this.omniJsonData.Case.CustomerDetails?.thirdPartyAddress ===
+        "Manual Address Entry" &&
+      (!Object.prototype.hasOwnProperty.call(
+        this.omniJsonData.Case.CustomerDetails,
+        "thirdPartyPostCode"
+      ) ||
+        thirdPartyCode === "" ||
+        !this.omniJsonData.Case?.validAddress)
+    ) {
+      return true;
+    }
+    if (
+      (this.omniJsonData.Case.CustomerDetails?.SearchAddressRadio ===
+        "Manual Address Entry" &&
+        !postCode.match(compare) &&
+        !["Overseas", "Not Applicable"].includes(postCode)) ||
+      (this.omniJsonData.Case.CustomerDetails?.thirdPartyAddress ===
+        "Manual Address Entry" &&
+        !thirdPartyCode.match(compare) &&
+        !["Overseas", "Not Applicable"].includes(thirdPartyCode))
     ) {
       return true;
     }
