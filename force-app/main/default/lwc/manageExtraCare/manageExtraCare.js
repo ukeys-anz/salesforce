b7ee8f44d52c203d1ab64c329892bd32d46f55ab
karencvaleriano <110364916+karencvaleriano@users.noreply.github.com>
2025-11-18 12:18:01 +1100

    [AR-249720] Fix Review Date to consider AEDT (#20454)

--- force-app/main/default/lwc/manageExtraCare/manageExtraCare.js
+++ force-app/main/default/lwc/manageExtraCare/manageExtraCare.js
@@ -360,11 +360,8 @@ export default class ManageExtraCare extends LightningElement {
   }
 
   updateReviewDate() {
-    let today = new Date();
-    today.setDate(today.getDate() + 1);
-    let futureDate = new Date();
-    futureDate.setFullYear(today.getFullYear() + 1);
-    let oneYearFromToday = futureDate.toISOString().split("T")[0];
+    const oneYearFromToday = this.getNextYear();
+
     // Review Date is not visible
     if (!this.showReviewDate) {
       this.newAccount.ExtraCareReviewDate__c = null;
@@ -389,5 +386,25 @@ export default class ManageExtraCare extends LightningElement {
     if (!ecReasonAreSame && newEcReason.length > oldEcReason.length) {
       this.newAccount.ExtraCareReviewDate__c = oneYearFromToday;
     }
+  }
+
+  getNextYear() {
+    // Get current date in AEDT
+    const today = new Date();
+
+    // Add one year
+    const nextYear = new Date(today);
+    nextYear.setFullYear(today.getFullYear() + 1);
+
+    // Format in AEDT
+    const options = {
+      timeZone: "Australia/Melbourne", // AEDT time zone
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit"
+    };
+
+    const formatter = new Intl.DateTimeFormat("en-CA", options);
+    return formatter.format(nextYear);
   }
 }
