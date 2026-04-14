6eb8c7258318da1bc12d4ab82c91a06bbbd7e392
masjedif <farid.masjedi@anz.com>
2026-04-14 10:23:33 +1000

    Temporary change on package & jest setup

--- ci/jest-sa11y-setup.js
+++ ci/jest-sa11y-setup.js
@@ -1,3 +1,5 @@
 import { setup } from "@sa11y/jest";
 // Register the sa11y matcher
-setup({ autoCheckOpts: { runAfterEach: true, cleanupAfterEach: true } });
+// Disabled automatic checks due to Jest 30 compatibility issues with @sa11y/jest@6.13.0
+// Can still use expect(element).toBeAccessible() manually in tests
+setup({ autoCheckOpts: { runAfterEach: false, cleanupAfterEach: true } });
